5eb6ee2a37b432e7e15640358f9577be3d338f32
Chastity White Rose <chastitywhiterose@gmail.com>
2025-09-23 05:40:47 -0500

    Code Cookbook Update

--- code/std/args/args-wip.c
+++ code/std/args/args-wip.c
@@ -2,11 +2,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 int main(int argc, char **argv)
 {
  int x,s_length;
  char *s; /*this will be used later as the master string*/
+ char *tokens[0x100],*token,tokens_length,delim[]=" "; /*used for strtok later*/
  char *default_message="Run this program with arguments to display them to the console.";
+
+ int *stack,stack_length=0x100,stack_index=0,i; /*stack variables*/
 
  printf("argc==%d\n",argc);
 
@@ -20,7 +24,7 @@ int main(int argc, char **argv)
  s_length=0;
  while(x<argc)
  {
-  printf("argv[%i]: %s\n",x,argv[x]);
+  /*printf("argv[%i]: %s\n",x,argv[x]);*/
   s_length+=strlen(argv[x]);
   x++;
  }
@@ -31,7 +35,7 @@ int main(int argc, char **argv)
  /*length if we assume one space between each argument*/
  printf("Length of arguments including spaces is %d\n",s_length+argc-2);
 
- /*allocates require length plus a little extra*/
+ /*allocates required length for string plus a little extra*/
  s=(char*)malloc(argc+s_length*sizeof(*s));if(s==NULL){printf("Failed to allocate main string a\n");return(1);}
  
  /*time to build the string from all the arguments*/
@@ -55,7 +59,125 @@ int main(int argc, char **argv)
 
  printf("Length of final string: %d\n",s_length);
 
+ /*
+  now that the string is built, we will process it as numbers and words
+  the strtok function was made for this kind of thing
+ */
+
+ /*loop for printing the tokens and saving them to an array but nothing else*/
+ x=0;
+ token = strtok(s,delim);
+ while (token)
+ {
+  tokens[x]=token;
+  printf("token[%d]: %s\n",x,tokens[x]);
+  token = strtok(NULL, " ");
+  x++;
+ }
+ tokens_length=x;
+
+ printf("there are %d tokens\n",tokens_length);
+
+
+stack=(int*)malloc(stack_length*sizeof(*stack));if(stack==NULL){printf("Failed to allocate integer stack a\n");return(1);}
+ 
+
+
+
+ /*loop for processing the tokens as numbers or command words*/
+ x=0;
+ while (x<tokens_length)
+ {
+  printf("token[%d]: %s\n",x,tokens[x]);
+
+  if(isdigit(tokens[x][0]))
+  {
+   printf("token[%d]: %s might be a number\n",x,tokens[x]);
+
+   sscanf(tokens[x],"%i",&i);
+
+   printf("number found: %i\n",i);
+   printf("it has been added to the stack\n");
+
+   stack[stack_index]=i;
+   stack_index++;
+  }
+
+  if(!strcmp(tokens[x],"add"))
+  {
+   printf("The add command adds the top two numbers on the stack.\n");
+   if(stack_index>=2)
+   {
+    stack[stack_index-2]+=stack[stack_index-1];
+    stack_index--;
+   }
+   else
+   {
+    printf("Error: Less than 2 numbers are on the stack. add command failed!\n");
+   }
+  }
+
+
+  if(!strcmp(tokens[x],"mul"))
+  {
+   printf("The mul command multiplies the top two numbers on the stack.\n");
+   if(stack_index>=2)
+   {
+    stack[stack_index-2]*=stack[stack_index-1];
+    stack_index--;
+   }
+   else
+   {
+    printf("Error: Less than 2 numbers are on the stack. mul command failed!\n");
+   }
+  }
+
+
+  if(!strcmp(tokens[x],"sub"))
+  {
+   printf("The sub command subtracts the top two numbers on the stack.\n");
+   if(stack_index>=2)
+   {
+    stack[stack_index-2]-=stack[stack_index-1];
+    stack_index--;
+   }
+   else
+   {
+    printf("Error: Less than 2 numbers are on the stack. sub command failed!\n");
+   }
+  }
+
+
+
+  x++;
+ }
+
+ printf("stack_index==%d\n",stack_index);
+
+
+
+
+ x=0;
+ while (x<stack_index)
+ {
+  printf("stack[%d]==%i\n",x,stack[x]);
+  x++;
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
  if(s!=NULL){free(s); s=NULL;}
+ if(stack!=NULL){free(stack); s=NULL;}
 
  return 0;
 }
