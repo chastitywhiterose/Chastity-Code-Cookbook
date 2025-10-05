3afa3708d2bf2c1b948e95dfc5e14209ada89252
Chastity White Rose <chastitywhiterose@gmail.com>
2025-10-05 05:19:02 -0500

    Code Cookbook Update

--- work/main.c
+++ work/main.c
@@ -1,40 +1,44 @@
-/* gcc fhex.c -o fhex -Wall -ansi -pedantic && ./fhex */
-
 #include <stdio.h>
 #include <stdlib.h>
+#include "chasteint.h"
 
-#define usl 256
-char us[usl+1]; /*global string which will be used to store string of integers*/
-
-char* ltostr(long i,int base,int width)
+void hexdump(FILE* fp)
 {
- char *s=us+usl;
- *s=0;
- do
+ int x,c,address=0,width=16;
+ x=0;
+ while ((c = fgetc(fp)) != EOF)
  {
-  s--;
-  *s=i%base;
-  i/=base;
-  if(*s<10){*s+='0';}else{*s=*s-10+'A';}
-  width--;
+  if(x%width==0)
+  {
+   printf("%08X: ",address);
+  }
+  
+  printf("%02X",c);
+  x++;
+
+  if(x==width){printf("\n");x=0;}
+  else{printf(" ");}
+   
+  address++;
  }
- while(i!=0 || width>0);
- return s;
+ if(x!=width){printf("\n");}
 }
  
 int main(int argc, char *argv[])
 {
- long argx,x,c;
+ int argx,x,c;
  FILE* fp; /*file pointer*/
    
  /*printf("argc=%i\n",argc);*/
 
  if(argc==1)
  {
+  printf("Chastity's Hexadecimal Tool\n\n");
   printf("This program reads or writes bytes of a file.\n\n");
-  printf("Read Usage:\n %s file address\n\n",argv[0]);
-  printf("Write Usage:\n %s file address byte\n\n",argv[0]);
-  return 1;
+  printf("Read Usage Type 1:\n %s file (hexdump entire file)\n\n",argv[0]);
+  printf("Read Usage Type 2:\n %s file address (read one byte from this address)\n\n",argv[0]);
+  printf("Write Usage:\n %s file address byte (write byte(s) to this address)\n",argv[0]);
+  return 0;
  }
 
  if(argc>1)
@@ -42,10 +46,8 @@ int main(int argc, char *argv[])
   fp=fopen(argv[1],"rb+");
   if(fp==NULL)
   {
-   printf("File %s does not exist.\n",argv[1]);
-   printf("Attempting to create file\n");
-   fp=fopen(argv[1],"wb+");
-   if(fp==NULL){printf("Failed to create file \"%s\".\n",argv[1]);}
+   printf("File \"%s\" does not exist.\n",argv[1]);
+   return 1;
   }
   else
   {
@@ -53,17 +55,22 @@ int main(int argc, char *argv[])
   }
  }
 
+ if(argc==2)
+ {
+  hexdump(fp); /*hex dump if only filename given*/
+ }
+
  if(argc>2)
  {
-  x=strtol(argv[2],NULL,16);
+  x=strint(argv[2],16);
   fseek(fp,x,SEEK_SET);
  }
 
  if(argc==3)
  {
   c=fgetc(fp);
-  printf("%s: ",ltostr(x,16,8));
-  if(c==EOF){printf("EOF\n");}else{printf("%s\n",ltostr(c,16,2));}
+  printf("%s: ",intstr(x,16,8));
+  if(c==EOF){printf("EOF\n");}else{printf("%s\n",intstr(c,16,2));}
  }
 
  if(argc>3)
@@ -72,33 +79,14 @@ int main(int argc, char *argv[])
   while(argx<argc)
   {
    c=strtol(argv[argx],NULL,16);
-   printf("%s: ",ltostr(x,16,8));
-   if(c==EOF){printf("EOF\n");}else{printf("%s\n",ltostr(c,16,2));}
+   printf("%s: ",intstr(x,16,8));
+   if(c==EOF){printf("EOF\n");}else{printf("%s\n",intstr(c,16,2));}
    fputc(c,fp);
    x++;
    argx++;
   }
  }
  
-
  fclose(fp);
  return 0;
 }
-
-
-/*
-This program reads or writes bytes of a file.
-
-To read a byte of a file:
-enter the name and the hex address to read.
-
-./fbyte 0.txt 0
-
-To write instead of reading:
-follow the address with hex byte values
-
-./fbyte 0.txt 0 12 34 56
-
-This works for any file as long as the file already exists to begin with.
-The name 0.txt is only an example. I used a text file with this name during testing.
-*/
