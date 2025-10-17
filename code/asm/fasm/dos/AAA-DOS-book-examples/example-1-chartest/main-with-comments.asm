c0b31c6cfcff85076c7789fd16e28cf2ba7b9eb3
Chastity White Rose <chastitywhiterose@gmail.com>
2025-10-16 21:09:21 -0500

    Code Cookbook Update

--- code/asm/fasm/dos/AAA-DOS-book-examples/example-1-chartest/main-with-comments.asm
+++ code/asm/fasm/dos/AAA-DOS-book-examples/example-1-chartest/main-with-comments.asm
@@ -1,12 +1,12 @@
-org 100h          ;tell assembler that code begins running at this address
+org 100h       ;tell assembler that code begins running at this address
 
-mov ah,2          ; move (copy) the number 2 into the ah register
-mov dl,20h        ; move the number 20 hex = 32 dec into the dl register
-start_of_loop:    ; a label that we will be jumping back to as the start of a loop
-int 21h           ; interrupt 21 hex = 33 dec starts a DOS system call
-add dl,1          ; add 1 to the dl register
-cmp dl,7Fh        ; compare the dl register with 7F hex = 127 dec
-jne start_of_loop ; Jump if Not Equal at the comparison above to restart the loop
+mov ah,2       ; move (copy) the number 2 into the ah register
+mov dl,20h     ; move the number 20 hex=32 dec into the dl register
+loop_start:    ; the loop starts here
+int 21h        ; interrupt 21 hex=33 dec for a DOS system call
+inc dl         ; add 1 to the dl register
+cmp dl,7Fh     ; compare the dl register with 7F hex = 127 dec
+jne loop_start ; Jump if Not Equal to loop_start
 
-mov ah,0          ; move zero into the ah register for the program exit call
-int 21h           ; DOS system call again but with a different ah value than before.
+mov ax,4C00h   ; DOS exit call with ah=4C and return al=0
+int 21h        ; DOS system call to complete the exit function
