38636c71c3ec8a6eaaffef5f489ba2fcaf6718cf
Chastity White Rose <chastitywhiterose@gmail.com>
2025-10-19 07:19:32 -0500

    Code Cookbook Update

--- code/asm/fasm/dos/AAA-DOS-book-examples/example-2-putstring/main.asm
+++ code/asm/fasm/dos/AAA-DOS-book-examples/example-2-putstring/main.asm
@@ -10,30 +10,40 @@ int 21h
 
 text db 'Hello World!',0Dh,0Ah,0
 
-; This section is for the putstring function I wrote.
-; It will print any zero terminated string that register ax points to
+;This section is for the putstring function I wrote.
+;It will print any zero terminated string that register ax points to
 
-stdout dw 1 ; value of standard output
+stdout dw 1 ; variable for standard output so that it can theoretically be redirected
 
 putstring:
 
-mov bx,ax ; copy ax to bx as well. Now both registers have the address of the main_string
+push ax
+push bx
+push cx
+push dx
 
-putstring_strlen_start: ; this loop finds the length of the string as part of the putstring function
+mov bx,ax                  ;copy ax to bx for use as index register
 
-cmp [bx], byte 0 ; compare this byte byte at address with 0
-jz putstring_strlen_end ; if comparison was zero, jump to loop end because we have found the length
-inc bx
-jmp putstring_strlen_start
+putstring_strlen_start:    ;this loop finds the length of the string as part of the putstring function
+
+cmp [bx], byte 0           ;compare this byte with 0
+jz putstring_strlen_end    ;if comparison was zero, jump to loop end because we have found the length
+inc bx                     ;increment bx (add 1)
+jmp putstring_strlen_start ;jump to the start of the loop and keep trying until we find a zero
 
 putstring_strlen_end:
 
-sub bx,ax ; sub ax from bx to get the difference for number of bytes
-mov cx,bx ; mov bx to cx
-mov dx,ax  ; dx will have address of string to write
+sub bx,ax                  ; sub ax from bx to get the difference for number of bytes
+mov cx,bx                  ; mov bx to cx
+mov dx,ax                  ; dx will have address of string to write
 
-mov ah,40h ; select DOS function 40h write 
-mov bx,[stdout]   ; file handle 1=stdout
-int 21h    ; call the DOS kernel
+mov ah,40h                 ; select DOS function 40h write 
+mov bx,[stdout]            ; file handle 1=stdout
+int 21h                    ; call the DOS kernel
 
-ret ; return to calling location
+pop dx
+pop cx
+pop bx
+pop ax
+
+ret
