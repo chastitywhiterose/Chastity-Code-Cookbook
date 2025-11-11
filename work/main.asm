344fd41b4e3163abb422bb2032310dcb1288746d
Chastity White Rose <chastitywhiterose@gmail.com>
2025-11-11 09:38:35 -0600

    Code Cookbook Update

--- work/main.asm
+++ work/main.asm
@@ -5,61 +5,57 @@ mov word [radix],10
 mov word [int_width],1
 mov [int_newline],0
 
-mov cx,0
-
-mov [array],byte 1
-
-powers_of_two:
-
-;this section prints the digits
-mov bx,[length]
-array_print:
-dec bx
-mov ax,0
-mov al,[array+bx]
+;the only even prime is 2
+mov ax,2
 call putint
-cmp bx,0
-jnz array_print
-call putline
+call putspace
 
-;this section adds the digits
-mov dl,0
+;fill array with zeros up to length
 mov bx,0
-array_add:
-mov ax,0
-mov al,[array+bx]
-add al,al
-add al,dl
-mov dl,0
-cmp al,10
-jb less_than_ten
-
-sub al,10
-mov dl,1
-
-less_than_ten:
-mov [array+bx],al
+array_zero:
+mov [array+bx],0
 inc bx
-cmp bx,[length]
-jnz array_add
+cmp bx,length
+jb array_zero
 
-cmp dl,0
-jz carry_is_zero
+;start by filtering multiples of first odd prime: 3
+mov ax,3
 
-mov [array+bx],1
-inc [length]
+primes:
 
-carry_is_zero:
+;print this number because it is prime
+call putint
+call putspace
 
-;keeps track of how many times the loop has run
-add cx,1
-cmp cx,64
-jna powers_of_two
+mov bx,ax ;mov ax to bx as our array index variable
+mov cx,ax ;mov ax to cx
+add cx,cx ;add cx to itself
+
+sieve:
+mov [array+bx],1 ;mark element as multiple of prime
+add bx,cx ;check only multiples of prime times 2 to exclude even numbers
+cmp bx,length
+jb sieve
+
+;check odd numbers until we find unused one not marked as multiple of prime
+mov bx,ax
+next_odd:
+add bx,2
+cmp [array+bx],0
+jz prime_found
+cmp bx,length
+jb next_odd
+prime_found:
+
+;get next prime read to print in ax
+mov ax,bx
+cmp ax,length
+jb primes
 
 mov ax,4C00h
 int 21h
 
-length dw 1
-array db 32 dup 0
-
 include 'chastelib16.asm'
+
+length=1000
+array rb length
