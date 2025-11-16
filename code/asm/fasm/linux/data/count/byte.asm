55afed45a3e1160103d4bb0f70cca404b019cc87
Chastity White Rose <chastitywhiterose@gmail.com>
2025-11-16 05:31:24 -0600

    Code Cookbook Update

--- code/asm/fasm/linux/data/count/byte.asm
+++ code/asm/fasm/linux/data/count/byte.asm
@@ -1,8 +1,5 @@
-; example of declaring bytes of data in a fasm source file
-; run with: fasm bytes.asm && hexdump -C bytes.bin
-
 a=0
-while a<256
+while a < 1 shl 8 
 db a
 a=a+1
 end while
---
+++ code/asm/fasm/linux/data/count/word.asm
@@ -0,0 +1,5 @@
+a=0
+while a < 1 shl 16 
+dw a
+a=a+1
+end while
