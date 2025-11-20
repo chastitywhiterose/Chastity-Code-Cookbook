1def063caa9fbf57bf685077c59ede342d7a1338
Chastity White Rose <chastitywhiterose@gmail.com>
2025-11-19 23:59:51 -0600

    Code Cookbook Update

--- code/asm/fasm/linux/elf/doc/chastity-ELF-create.asm
+++ code/asm/fasm/linux/elf/doc/chastity-ELF-create.asm
@@ -2,7 +2,7 @@
 ;
 ;All data as defined in this file is basd off of the specification of the ELF file format.
 ;I first looked at the type of file created by FASM's "format ELF executable" directive.
-;It is great that FASM can create an executable file automatically.
+;It is great that FASM can create an executable file automatically. (Thanks Tomasz Grysztar, you are a true warrior!)
 ;However, I wanted to understand the format for use in other assemblers like NASM.
 
 ;The Github repository with the spec I used is here.
@@ -45,21 +45,24 @@ dd 0x8048000   ;p_paddr: Physical address. Same as previous
 
 ;not sure about these two values yet
 
-image_size=0x1000 ;Chosen size for file and memory size.
+image_size=0x1000 ;Chosen size for file and memory size. At minimum this must be as big as the actual binary file (code after header included)
+                  ;By choosing a default size of 0x1000, I am assuming all assembly programs I write will be less than 4 kilobytes
 
-dd image_size       ;Elf32_Word p_filesz: Size of file image of the segment. Must be equal to the file size or greater
-dd image_size      ;Elf32_Word p_memsz: This member gives the number of bytes in the memory image of the segment; it may be zero.
+dd image_size  ;p_filesz: Size of file image of the segment. Must be equal to the file size or greater
+dd image_size  ;p_memsz: Size of memory image of the segment, which may be equal to or greater than file image.
 
-dd 7           ;Elf32_Word p_flags; 7=4(Read)+2(Write)+1(Execute)
-dd 0x1000      ;Elf32_Word p_align; Alignment
+dd 7           ;p_flags: permission flags: 7=4(Read)+2(Write)+1(Execute)
+dd 0           ;p_align; Alignment (none)
 
-use32 ;tell assembler that 32 bit code is being used
-org e_entry ;origin of new code begins at the entry point
+;important FASM directives
+use32          ;tell assembler that 32 bit code is being used
+org e_entry    ;origin of new code begins at the entry point
 
-mov eax,4   ; invoke SYS_WRITE (kernel opcode 4 on 32 bit systems)
-mov ebx,1   ; write to the STDOUT file
-mov ecx,msg ; pointer/address of string to write
-mov edx,13  ; number of bytes to write
+;now, the actual hello world program
+mov eax,4      ;invoke SYS_WRITE (kernel opcode 4 on 32 bit systems)
+mov ebx,1      ;write to the STDOUT file
+mov ecx,msg    ;pointer/address of string to write
+mov edx,13     ;number of bytes to write
 int 80h
 
 mov eax,1 ;function SYS_EXIT (kernel opcode 1 on 32 bit systems)
@@ -68,17 +71,6 @@ int 80h   ;call Linux kernel with interrupt
 
 msg db 'Hello World!',0Ah
 
-;chastity@chastity-um250:~/git/Chastity-Code-Cookbook/code/asm/fasm/linux/elf$ hd ./elf-new-hello
-;00000000  7f 45 4c 46 01 01 01 00  00 00 00 00 00 00 00 00  |.ELF............|
-;00000010  02 00 03 00 01 00 00 00  54 80 04 08 34 00 00 00  |........T...4...|
-;00000020  00 00 00 00 00 00 00 00  34 00 20 00 01 00 28 00  |........4. ...(.|
-;00000030  00 00 00 00 01 00 00 00  00 00 00 00 00 80 04 08  |................|
-;00000040  00 80 04 08 83 00 00 00  83 00 00 00 07 00 00 00  |................|
-;00000050  00 10 00 00 b8 04 00 00  00 bb 01 00 00 00 b9 76  |...............v|
-;00000060  80 04 08 ba 0d 00 00 00  cd 80 b8 01 00 00 00 bb  |................|
-;00000070  00 00 00 00 cd 80 48 65  6c 6c 6f 20 57 6f 72 6c  |......Hello Worl|
-;00000080  64 21 0a                                          |d!.|
-;00000083
 
 
 
