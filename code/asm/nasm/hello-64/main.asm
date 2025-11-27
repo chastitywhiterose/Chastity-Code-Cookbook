5f7a9a31e61f396ad3a05aaee3e77c9e9235058a
Chastity White Rose <chastitywhiterose@gmail.com>
2025-11-27 05:53:46 -0600

    Code Cookbook Update

---
+++ code/asm/nasm/hello-64/main.asm
@@ -0,0 +1,18 @@
+;format ELF64 executable
+;entry main
+
+use64
+
+main:
+
+mov rax,1   ; invoke SYS_WRITE (kernel opcode 1 on 64 bit systems)
+mov rdi,1   ; write to the STDOUT file
+mov rsi,msg ; pointer/address of string to write
+mov rdx,13  ; number of bytes to write
+syscall
+
+mov rax,60  ; invoke SYS_EXIT (kernel opcode 60 on 64 bit systems)
+mov rdi,0   ; return 0 status on exit - 'No Errors'
+syscall
+
+msg db 'Hello World!', 0Ah ; assign msg variable with your message string
