a4028199c4400c396c8ddb1433efdc960b96a3ab
Chastity White Rose <chastitywhiterose@gmail.com>
2025-12-03 23:32:19 -0600

    Code Cookbook Update

---
+++ work/main.asm
@@ -0,0 +1,289 @@
+;Linux 64-bit Assembly Source for chastehex
+;a special tool originally written in C
+format ELF64 executable
+entry main
+
+include 'chastelib64.asm'
+include "chasteio64.asm"
+
+main:
+
+;radix will be 16 because this whole program is about hexadecimal
+mov [radix],16 ; can choose radix for integer input/output!
+mov [int_newline],0 ;disable automatic printing of newlines after putint
+;we will be manually printing spaces or newlines depending on context
+
+pop rax
+mov [argc],eax ;save the argument count for later
+
+;first arg is the name of the program. we skip past it
+pop rax
+dec [argc]
+
+;before we try to get the first argument as a filename, we must check if it exists
+cmp [argc],0
+jnz arg_open_file
+
+help:
+mov eax,help_message
+call putstring
+jmp main_end
+
+arg_open_file:
+
+pop rax
+dec [argc]
+mov [filename],rax ; save the name of the file we will open to read
+call putstring
+call putline
+
+call open
+
+cmp eax,0
+js main_end
+
+mov [filedesc],rax ; save the file descriptor number for later use
+mov [file_offset],0 ;assume the offset is 0,beginning of file
+
+;check next arg
+cmp [argc],0 ;if there are no more args after filename, just hexdump it
+jnz next_arg_address ;but if there are more, jump to the next argument to process it as address
+
+hexdump:
+
+mov rdx,0x10         ;number of bytes to read
+mov rsi,byte_array   ;address to store the bytes
+mov rdi,[filedesc]   ;move the opened file descriptor into rdi
+mov rax,0            ;invoke SYS_READ (kernel opcode 0 on 64 bit Intel)
+syscall              ;call the kernel
+
+mov [bytes_read],rax
+
+; call putint
+
+cmp rax,0
+jnz file_success ;if more than zero bytes read, proceed to display
+
+;if the offset is zero, display EOF to indicate empty file
+;otherwise, end without displaying this because there should already be bytes printed to the display
+cmp [file_offset],0
+jnz main_end
+
+call show_eof
+
+jmp main_end
+
+; this point is reached if file was read from successfully
+
+file_success:
+;mov eax,[filename]
+;call putstring
+;mov eax,file_opened_string
+;call putstring
+
+mov eax,byte_array
+;call putstring
+
+call print_bytes_row
+
+cmp [bytes_read],1 
+jl main_end ;if less than one bytes read, there is an error
+jmp hexdump
+
+;address argument section
+next_arg_address:
+
+;if there is at least one more arg
+pop rax ;pop the argument into eax and process it as a hex number
+dec [argc]
+call strint
+
+mov rdx,0          ;whence argument (SEEK_SET)
+mov rsi,rax        ;move the file cursor to this address
+mov rdi,[filedesc] ;move the opened file descriptor into rdi
+mov rax,8          ;invoke SYS_LSEEK (kernel opcode 8 on 64 bit Intel)
+syscall            ;call the kernel
+
+mov [file_offset],eax ;move the new offset
+
+;check the number of args still remaining
+cmp [argc],0
+jnz next_arg_write ; if there are still arguments, skip this read section and enter writing mode
+
+read_one_byte:
+mov edx,1          ;number of bytes to read
+mov ecx,byte_array ;address to store the bytes
+mov ebx,[filedesc] ;move the opened file descriptor into EBX
+mov eax,3          ;invoke SYS_READ (kernel opcode 3)
+int 80h            ;call the kernel
+
+;eax will have the number of bytes read after system call
+cmp eax,1
+jz print_byte_read ;if exactly 1 byte was read, proceed to print info
+
+call show_eof
+
+jmp main_end ;go to end of program
+
+;print the address and the byte at that address
+print_byte_read:
+call print_byte_info
+
+;this section interprets the rest of the args as bytes to write
+next_arg_write:
+cmp [argc],0
+jz main_end
+
+pop rax
+dec [argc]
+call strint ;try to convert string to a hex number
+
+;write that number as a byte value to the file
+
+mov [byte_array],al
+
+mov rdx,1          ;write 1 byte
+mov rsi,byte_array ;pointer/address of byte to write
+mov rdi,[filedesc] ;write to this file descriptor
+mov rax,1          ;invoke SYS_WRITE (kernel opcode 1 on 64 bit systems)
+syscall            ;system call to write the message
+
+call print_byte_info
+inc [file_offset]
+
+jmp next_arg_write
+
+main_end:
+
+;this is the end of the program
+;we close the open file and then use the exit call
+
+mov eax,[filedesc] ;file number to close
+call close
+
+mov rax, 0x3C ; invoke SYS_EXIT (kernel opcode 0x3C (60 decimal) on 64 bit systems)
+mov rdi,0   ; return 0 status on exit - 'No Errors'
+syscall
+
+
+;this function prints a row of hex bytes
+;each row is 16 bytes
+print_bytes_row:
+mov eax,[file_offset]
+mov [int_width],8
+call putint
+call putspace
+
+mov ebx,byte_array
+mov ecx,[bytes_read]
+add [file_offset],ecx
+next_byte:
+mov eax,0
+mov al,[ebx]
+mov [int_width],2
+call putint
+call putspace
+
+inc ebx
+dec ecx
+cmp ecx,0
+jnz next_byte
+
+mov ecx,[bytes_read]
+pad_spaces:
+cmp ecx,0x10
+jz pad_spaces_end
+mov eax,space_three
+call putstring
+inc ecx
+jmp pad_spaces
+pad_spaces_end:
+
+;optionally, print chars after hex bytes
+call print_bytes_row_text
+call putline
+
+ret
+
+space_three db '   ',0
+
+print_bytes_row_text:
+mov ebx,byte_array
+mov ecx,[bytes_read]
+next_char:
+mov eax,0
+mov al,[ebx]
+
+;if char is below '0' or above '9', it is outside the range of these and is not a digit
+cmp al,0x20
+jb not_printable
+cmp al,0x7E
+ja not_printable
+
+printable:
+;if char is in printable range,copy as is and proceed to next index
+jmp next_index
+
+not_printable:
+mov al,'.' ;otherwise replace with placeholder value
+
+next_index:
+mov [ebx],al
+inc ebx
+dec ecx
+cmp ecx,0
+jnz next_char
+mov [ebx],byte 0 ;make sure string is zero terminated
+
+mov eax,byte_array
+call putstring
+
+ret
+
+
+;function to display EOF with address
+show_eof:
+
+mov eax,[file_offset]
+mov [int_width],8
+call putint
+call putspace
+mov eax,end_of_file_string
+call putstring
+call putline
+
+ret
+
+;print the address and the byte at that address
+print_byte_info:
+mov eax,[file_offset]
+mov [int_width],8
+call putint
+call putspace
+mov eax,0
+mov al,[byte_array]
+mov [int_width],2
+call putint
+call putline
+
+ret
+
+end_of_file_string db 'EOF',0
+
+help_message db 'Welcome to chastehex! The tool for reading and writing bytes of a file!',0Ah,0Ah
+db 'To hexdump an entire file:',0Ah,0Ah,9,'chastehex file',0Ah,0Ah
+db 'To read a single byte at an address:',0Ah,0Ah,9,'chastehex file address',0Ah,0Ah
+db 'To write a single byte at an address:',0Ah,0Ah,9,'chastehex file address value',0Ah,0Ah,0
+
+;variables for managing arguments
+argc dd 0
+filename dq 0 ; name of the file to be opened
+filedesc dq 0 ; file descriptor
+bytes_read dq 0
+file_offset dq 0
+
+
+
+
+;where we will store data from the file
+byte_array db 17 dup ?
