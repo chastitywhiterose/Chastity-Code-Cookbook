a37c08764105544696b576ddd0a207b3b7aec66a
Chastity White Rose <chastitywhiterose@gmail.com>
2025-12-07 07:15:58 -0600

    Code Cookbook Update

--- code/asm/nasm/chastelib/main.asm
+++ code/asm/nasm/chastelib/main.asm
@@ -1,29 +1,18 @@
 section	.data
-
-%include 'putstring32.asm'
-
-%include 'putint32.asm'
-
-
+%include 'chastelib32.asm'
 global  _start
- 
+
 _start:
-
-mov  [radix], dword 10 ; can choose radix for integer output!
-mov dword [int_width],1
-
+	mov dword [radix],10    ; can choose radix for integer output!
+	mov dword [int_width],1
 	mov eax,1987
 	call putint
  
 	mov eax,msg
 	call putstring
 
-
 	mov ebx, 0      ; return 0 status on exit - 'No Errors'
 	mov eax, 1      ; invoke SYS_EXIT (kernel opcode 1)
 	int 80h
 
-
-
 	msg db 'Hello World!', 0Ah,0     ; assign msg variable with your message string
-
