84898b50bde18486f1ac3c8b29f997eb499c8c82
Chastity White Rose <chastitywhiterose@gmail.com>
2025-12-13 22:06:26 -0600

    Code Cookbook Update

---
+++ work/main.asm
@@ -0,0 +1,51 @@
+format ELF executable
+entry main
+
+include 'chastelib32.asm'
+
+main: ; the main function of our assembly function, just as if I were writing C.
+
+; I can load any string address into eax and print it!
+
+mov ebx, 1 ;ebx must be 1 to write to standard output
+
+mov eax,msg
+call putstring
+mov eax,main_string ; move the address of main_string into eax register
+call putstring
+
+mov [radix],2 ; can choose radix for integer output!
+mov [int_width],8
+
+mov eax,0
+loop1:
+call putint
+inc eax
+cmp eax,10h;
+jnz loop1
+
+mov eax,test_int
+call strint
+
+mov [radix],10 ; Choose radix 10 for integer output!
+mov [int_width],8
+call putint
+
+
+mov eax, 1  ; invoke SYS_EXIT (kernel opcode 1)
+mov ebx, 0  ; return 0 status on exit - 'No Errors'
+int 80h
+
+; this is where I keep my string variables
+
+msg db 'Hello World!', 0Ah,0     ; assign msg variable with your message string
+main_string db "This is Chastity's 32-bit Assembly Language counting program!",0Ah,0
+; test string of integer for input
+test_int db '10011101001110011110011',0
+
+; This Assembly source file has been formatted for the FASM assembler.
+; The following 3 commands assemble, give executable permissions, and run the program
+;
+;	fasm main.asm
+;	chmod +x main
+;	./main
--- work/main.s
+++ work/main.s
@@ -1,13 +1,20 @@
 .data
 title: .asciiz "Modifying a string in memory!\n"
 
+#this is the location in memory where digits are written to by the putint function
+int_string: .byte '?':32
+int_newline: .byte 10,0
+
 .text
 main:
 
-li $t0,'?'
-la $t1,title
-addi $t2,$t1,6 # $t0=$t0+1
+li $t0,'0'
+la $t1,int_newline
+addi $t1,$t1,-1
 
+#sb $t0,($t1)
+
+;lb $t0,1($t1)
 
 loop1:
 sb $t0,12($t1)
@@ -17,7 +24,7 @@ blt $t1,$t2,loop1
 
 #print string
 li $v0,4      # load immediate, v0 = 4 (4 is print string system call)
-la $a0,title  # load address of string to print into a0
+la $a0,int_string  # load address of string to print into a0
 syscall
 
 li   $v0, 10     # exit syscall
