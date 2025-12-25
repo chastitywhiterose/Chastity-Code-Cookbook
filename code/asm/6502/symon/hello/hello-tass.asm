3b345baff84d95f8cc8ee2e428617b50b2deb1c8
Chastity White Rose <chastitywhiterose@gmail.com>
2025-12-25 01:54:00 -0600

    Code Cookbook Update

---
+++ code/asm/6502/symon/hello/hello-tass.asm
@@ -0,0 +1,28 @@
+; 64tass hello-tass.asm -o hello-tass.bin -b
+
+iobase   = $8800
+iostatus = iobase + 1
+iocmd    = iobase + 2
+ioctrl   = iobase + 3
+
+* = $300 ; same as org $300 in other assemblers
+
+main:   cli
+        lda #$0b
+        sta iocmd      ; Set command status
+        lda #$1a
+        sta ioctrl     ; 0 stop bits, 8 bit word, 2400 baud
+
+init:   ldx #$00       ; Initialize index
+
+loop:   lda iostatus
+        and #$10       ; Is the tx register empty?
+        beq loop       ; No, wait for it to empty
+        lda string,x   ; Otherwise, load the string pointer
+        beq init       ; If the char is 0, re-init
+        sta iobase     ; Otherwise, transmit
+
+        inx            ; Increment string pointer.
+        jmp loop       ; Repeat write.
+
+string: .text "Hello, 6502 world! ", 0
