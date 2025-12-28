d081e9b7639e044f7a66055dd2ffa6fe471d167d
Chastity White Rose <chastitywhiterose@gmail.com>
2025-12-28 01:42:32 -0600

    Code Cookbook Update

--- code/rust/functions/putint/main.rs
+++ code/rust/functions/putint/main.rs
@@ -1,17 +1,25 @@
+/*
+ chastelib: Rust edition
+
+ The main function of this program calls the functions of chastelib. chastelib is a library of functions for integer input and output using any base from 2 to 36. It is also possible to customize the width (how many leading zeros) an integer is printed with.
+*/
+
 fn main()
 {
- println!("This is a test of the putint function I wrote for the Rust programming language.");
- let mut i:i32=0;
- while i<256
+ putstring("This program tests chastelib: Rust edition\n");
+ let mut a:u32=0;
+ let b=strint("100",16);
+ while a<b
  {
-  putint(i,2,8);
+  putint(a,2,8);
   print!(" ");
-  putint(i,16,2);
+  putint(a,16,2);
   print!(" ");
-  putint(i,10,3);
+  putint(a,10,3);
   println!();
-  i+=1;
+  a+=1;
  }
+
 }
 
 /*
@@ -32,11 +40,11 @@ There is probably a better way to write this function in Rust but this is the fi
 
 */
 
-fn putint(mut i:i32,radix:i32,int_width:usize)
+fn putint(mut i:u32,radix:u32,int_width:usize)
 {
  let mut a: [u8;32]=[0;32]; //create array of max size needed for 32 bit integer
  let mut width=0; //keeps track of current width of integer (how many digits in the chosen radix)
- let mut r:i32; //used to store the remainder of division
+ let mut r:u32; //used to store the remainder of division
 
  while i!=0 || width<int_width
  {
@@ -53,4 +61,37 @@ fn putint(mut i:i32,radix:i32,int_width:usize)
   width-=1;
   print!("{}", a[width] as char );
  }
+}
+
+
+/*
+ this function converts a string in the specified radix into an actual integer
+ Because a string can be converted into bytes with the "as_bytes()" method, I was able to get this function working identically to the way it does in the C version. Prefixing a char literal with a b turns it into a u8 byte literal. The comparisons were therefore possible with minor changes.
+*/
+
+fn strint(s:&str,radix:u32) -> u32
+{
+ let mut i:u32=0;
+ let bytes = s.as_bytes();
+ let mut x:usize=0;
+ let mut c:u8;
+ while x<bytes.len()
+ {
+  c=bytes[x];
+  if c >= b'0' && c <= b'9' {c-=b'0';}
+  else if c >= b'A' && c <= b'Z' {c-=b'A';c+=10;}
+  else if c >= b'a' && c <= b'z' {c-=b'a';c+=10;}
+  else if c == b' ' || c == b'\n' || c == b'\t' {return i;}
+  else{print!("Error: {} is not an alphanumeric character!\n",c);return i;}
+  if c>=radix as u8{print!("Error: {} is not a valid character for radix {}\n",c,radix);return i;}
+  i*=radix;
+  i+=c as u32;
+  x+=1;
+ }
+ return i;
+}
+
+fn putstring(s:&str)
+{
+ print!("{}",s);
 }
