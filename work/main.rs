f9273a39d0039e7acf49edfc6283162adb195a28
Chastity White Rose <chastitywhiterose@gmail.com>
2025-12-27 22:50:21 -0600

    Code Cookbook Update

--- work/main.rs
+++ work/main.rs
@@ -11,17 +11,31 @@ fn main()
  println!("the number is {}",number);
 
  putint(number,16,8);
- 
+ println!();
 
+ println!("the number is {}",number);
+ 
 }
 
+/*
+ This is the putint function for printing an integer in any base from 2 to 36.
+ It is the same function I wrote in C and Assembly but with some key differences for Rust.
+
+ Rust doesn't allow global variables in "safe" mode. Therefore, the radix and the int_width must be passed to the function each time.
+ I find this inefficient because usually I am just choosing one radix for the duration of the entire program.
+ The width also typically stays the same unless I am doing something fancy, such as I did in chastehex.
+
+ The first loop stores the correct ASCII numbers as unsigned bytes in an array by repeatedly dividing by the radix and converting the remainder of division into u8 (unsigned 8 bit integer) after adding the correct numbers based on the ASCII table.
+
+ The second loop converts these ASCII numbers into the Rust (char) type and prints them in the reverse order of how they were stored.
+
+ Most of the code in this function was required only because Rust imposes limitations on what I can do because strings are not simply mutable arrays of bytes. Additionally the char type is not the same as the char type in C. In C, chars are the same as 1 byte but in Rust they are actually unicode characters that are 4 bytes each.
+
+*/
 
 fn putint(mut i:i32,radix:i32,int_width:usize)
 {
- println!("at the beginning of the function, the number is {}",i);
- println!("the selected radix is {}",radix);
-
- let mut a: [i32;32]=[0;32]; //create array of max size needed for 32 bit integer
+ let mut a: [u8;32]=[0;32]; //create array of max size needed for 32 bit integer
  let mut width=0; //keeps track of current width of integer (how many digits in the chosen radix)
  let mut r:i32; //used to store the remainder of division
 
@@ -29,18 +43,17 @@ fn putint(mut i:i32,radix:i32,int_width:usize)
  {
   r=i%radix;
   i/=radix;
-  a[width]=r;
+  if r<10 { r+=0x30 }
+  else {r+=0x37}
+  a[width]=r as u8;
   width+=1;
  }
 
  while width>0
  {
   width-=1;
-  if a[width]<10 { print!("{},",a[width]); }
-  else {print!("{},",(char)a[width]);}
+  print!("{}", a[width] as char );
  }
- println!();
-
 }
 
 
