c3fcd8a903bb5b9458fc21bbc1b01e95922ce46c
Chastity White Rose <chastitywhiterose@gmail.com>
2026-01-08 06:47:09 -0600

    Code Cookbook Update

---
+++ work/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+#include "chastelib.hpp"
+
+int main(int argc, char *argv[])
+{
+ int a=0,b;
+
+ radix=16;
+ int_width=1;
+
+ putstring("This program is the official test program for the C++ version of chastelib.\n");
+
+ b=strint("10");
+ while(a<b)
+ {
+  radix=2;
+  int_width=8;
+  putint(a);
+  putstring(" ");
+  radix=16;
+  int_width=2;
+  putint(a);
+  putstring(" ");
+  radix=10;
+  int_width=3;
+  putint(a);
+
+  if(a>=0x20 && a<=0x7E)
+  {
+   putstring(" ");
+   cout << a;
+  }
+
+  putstring("\n");
+  a+=1;
+ }
+  
+ return 0;
+}
