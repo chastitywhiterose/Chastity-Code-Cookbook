3a68d459a17ec0407330170d3f6a13e05c5f6dde
Chastity White Rose <chastitywhiterose@gmail.com>
2026-01-20 09:33:43 -0600

    Code Cookbook Update

---
+++ code/c/ansi/ansi-documentation/ANSI_Sequences.md
@@ -0,0 +1,10 @@
+# ANSI Escape Code Sequences
+
+ANSI escape codes allow moving the cursor and changing the color of text in a Linux terminal. The best part is that these sequences can be constructed by sending the correct byte sequence of characters no matter what programming language you are using. Technically, this means that even a library like ncurses can be constructed from knowing about them.
+
+<https://notes.burke.libbey.me/ansi-escape-codes/>  
+<https://gist.github.com/fnky/458719343aabd01cfb17a3a4f7296797>  
+
+My goals are very simple. I want to create a small text program involving cursor movement and some colors. Perhaps a hex editor or text editor will be good projects.
+
+However, if I have the time and skill, I would like to make a text mode game that is fun to play and is an original idea.
---
+++ work/main.c
@@ -0,0 +1,133 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "chastelib.h"
+#include "chastelib-ansi.h"
+
+char RAM[0x1000];
+int RAM_address=0;
+int RAM_view_x=0;
+int RAM_view_y=2;
+
+int byte_selected_x=0;
+int byte_selected_y=0;
+
+
+/*outputs the ASCII text to the right of the hex field*/
+void RAM_textdump(int y,int width)
+{
+ int a,x=0;
+ int count=16;
+ x=count;
+ while(x<0x10)
+ {
+  putstring("   ");
+  x++;
+ }
+
+ x=0;
+ while(x<count)
+ {
+  a=RAM[RAM_address+x+y*width];
+  if( a < 0x20 || a > 0x7E ){a='.';}
+  putchar(a);
+  x++;
+ }
+/* RAM[RAM_address+y*width]=0;*/
+
+ /*putstring(bytes);*/
+}
+
+void RAM_hexdump()
+{
+ int x,y;
+ int width=16,height=16;
+ radix=16;
+ 
+ y=0;
+ while(y<height)
+ {
+  text_rgb(0x00,0xFF,0xFF);
+  int_width=8;
+  radix=16;
+  putint(RAM_address+y*width);
+  putstring(" ");
+  text_rgb(0xFF,0x00,0xFF);
+  int_width=2;
+  x=0;
+  while(x<width)
+  {
+  
+   if(x==byte_selected_x&&y==byte_selected_y)
+   {
+    text_rgb(0x00,0xFF,0x00); /*change color before next byte is printed*/
+    putint(RAM[x+y*width]&0xFF); /*print this byte in the new color*/
+    text_rgb(0xFF,0x00,0xFF); /*change it back*/
+   }
+   else
+   {
+    putint(RAM[x+y*width]&0xFF);
+   }
+ 
+   putstring(" ");
+
+
+   x++;
+  }
+  text_rgb(0xFF,0xFF,0x00);
+  RAM_textdump(y,width);
+  
+  putstring("\n");
+  y++;
+  move_xy(RAM_view_x,RAM_view_y+y); radix=16;
+ }
+
+}
+
+int key=0;
+
+void input_operate()
+{
+ int width=16,height=16;
+ 
+ if(key=='A'){byte_selected_y--;if(byte_selected_y<0){byte_selected_y=15;}}
+ if(key=='B'){byte_selected_y++;if(byte_selected_y>=15){byte_selected_y=0;}}
+ if(key=='C'){byte_selected_x++;if(byte_selected_x>15){byte_selected_x=0;}}
+ if(key=='D'){byte_selected_x--;if(byte_selected_x<0){byte_selected_x=15;}}
+
+ if(key=='+'){RAM[byte_selected_x+byte_selected_y*width]++;}
+ if(key=='-'){RAM[byte_selected_x+byte_selected_y*width]--;}
+ 
+ 
+
+}
+
+int main(int argc, char *argv[])
+{
+
+ while(/*key!=0x1B&&*/key!='q')
+ {
+ 
+  text_rgb(0xFF,0xFF,0xFF);
+ 
+  putstring(ansi_clear);
+  putstring(ansi_home);
+ 
+  putchar(key);
+  putstring(" ");
+  move_xy(10,0);
+ 
+  radix=16;
+  putint(key);
+   
+  move_xy(RAM_view_x,RAM_view_y);
+
+  RAM_hexdump();
+ 
+  move_xy(0,0);
+  key=getchar();  /*fread(&key,1,1,stdin);*/
+  input_operate();
+  
+ }
+ 
+ return 0;
+}
