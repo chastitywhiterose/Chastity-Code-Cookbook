fae0ca2398aea6bfdbf70a3126ecab1d7eff83e6
Chastity White Rose <chastitywhiterose@gmail.com>
2026-01-24 16:15:37 -0600

    Code Cookbook Update

--- work/main.cpp
+++ work/main.cpp
@@ -5,18 +5,25 @@ using namespace std;
 /*
  This is probably a silly example of a class in C++, but since an integer is what I am most comfortable with
  I will make an integer class which contains only a single int which is really just the short name for an integer
+ The current value of the integer is public and can always be accessed as objectname.i
+ This class has functions to set the value of the integer by the set method and other methods to do arithmetic
 */
+
+
 
 class integer
 {
+ static int id; //the number that idenfifies the order in which this object was created*/
+
  public:
 
  int i;
 
  integer()
  {
-  cout << "integer set to default zero value\n";
+  cout << "integer id " << id << " set to default zero value\n";
   i=0;
+  id++;
  }
 
  void set(int a)
@@ -34,15 +41,35 @@ class integer
   cout << i << '\n';
  }
 
+ void add(int a) { i+=a; }
+
+ void sub(int a) { i-=a; }
+
+ void mul(int a) { i*=a; }
+
+ void div(int a) { i/=a; }
+
+ void mod(int a) { i%=a; }
+
 
 };
 
+int integer::id=0; //this is where the starting value for the static integer is declared
+
+
+
 int main(int argc, char *argv[])
 {
- integer a;
- a.set(6);
+ integer a,b;
 
- a.println();
+ a.set(1);
+ b.set(256);
+
+ while(a.i<b.i)
+ {
+  a.println();
+  a.mul(3);
+ }
 
  return 0;
 }
