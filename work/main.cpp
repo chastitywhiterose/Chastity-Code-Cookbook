1a4d78b266a65a1dc3ac9d48166ce45e00e33e31
Chastity White Rose <chastitywhiterose@gmail.com>
2026-01-24 05:06:53 -0600

    Code Cookbook Update

--- work/main.cpp
+++ work/main.cpp
@@ -2,6 +2,11 @@
 using namespace std;
 //#include "chastelib.hpp"
 
+/*
+ This is probably a silly example of a class in C++, but since an integer is what I am most comfortable with
+ I will make an integer class which contains only a single int which is really just the short name for an integer
+*/
+
 class integer
 {
  public:
@@ -11,16 +16,22 @@ class integer
  integer()
  {
   cout << "integer set to default zero value\n";
+  i=0;
  }
- 
- void operator=(int a)
+
+ void set(int a)
  {
   i=a;
  }
-
- void operator+=(int a)
+ 
+ void print()
  {
-  i+=a;
+  cout << i;
+ }
+
+ void println()
+ {
+  cout << i << endl;
  }
 
 
@@ -29,13 +40,9 @@ class integer
 int main(int argc, char *argv[])
 {
  integer a;
- a=6;
+ a.set(6);
 
- a+=7;
+ a.println();
 
- a=a+1;
-
-// a+=7;
- cout << a.i << "\n";
  return 0;
 }
