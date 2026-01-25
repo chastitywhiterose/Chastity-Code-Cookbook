f58542cc114d47068c810cbd810f9a371f434d1c
Chastity White Rose <chastitywhiterose@gmail.com>
2026-01-25 05:26:22 -0600

    Code Cookbook Update

--- work/chastebuf.h
+++ work/chastebuf.h
@@ -1,14 +1,21 @@
 /*
  chastebuf.h
  
+ Welcome to the chastebuf library which is the friend of the core chastelib library, but does not strictly require it.
+ This extension library was specifically made to improve performance for my project: "Hexplore" which is an interactive terminal hex editior
+ Since each time the screen is cleared and must be refreshed with all new data, having a fast buffered output system that I wrote and control is required
+ 
  This file contains some global variables and two utility functions.
  The idea is that you have data stored in a very large array to be printed. However, rather than calling putstring multiple times, you can call the bufcat
  function to store the strings into the buffer but not print them yet. Then, when it is time to print everything so far, you call bufput and everything is printed at once.
+ 
+ One important thing all these functions do is terminate the string with zero after they append the string or character to the buffer.
+ Another thing that is required is that when the buffer is printed with bufput, the bp pointer is set back to the beginning of the buffer.
 */
 
 char buffer[0x10000]; /*buffer of characters to store things we are not printing yet*/
 char *bp=buffer;    /*bp stands for buffer pointer*/
- 
+
 void bufcat(char *s)
 {
  while(*s)
---
+++ work/chastelib-ansi.h
@@ -0,0 +1,116 @@
+/*
+ this file is dedicated to using ANSI escape codes in Assembly programs.
+;Using special strings of text, it is possible to reposition the cursor in Linux terminal programs
+;Below are some links where I learned about ANSI escape sequences;
+
+https://notes.burke.libbey.me/ansi-escape-codes/
+https://gist.github.com/fnky/458719343aabd01cfb17a3a4f7296797
+*/
+
+char ansi_gray[]="\x1B[1;30m";
+char ansi_red[]="\x1B[1;31m";
+char ansi_green[]="\x1B[1;32m";
+char ansi_yellow[]="\x1B[1;33m";
+char ansi_blue[]="\x1B[1;34m";
+char ansi_magenta[]="\x1B[1;35m";
+char ansi_cyan[]="\x1B[1;36m";
+char ansi_white[]="\x1B[1;37m";
+
+char ansi_clear[]="\x1B[2J";
+
+char ansi_home[]="\x1B[H";
+
+
+int temp_radix;
+int temp_width;
+
+void save_radix()
+{
+ temp_radix=radix;
+ temp_width=int_width;
+}
+
+void load_radix()
+{
+ radix=temp_radix;
+ int_width=temp_width;
+}
+
+
+char ansi_string[0x100]; /*global string which will be used to build the move escape sequence*/
+
+void text_rgb(int r,int g,int b)
+{
+ char *si,*di;
+ save_radix();
+
+ 
+ di=ansi_string;
+ 
+ *di++=0x1B;
+ *di++='[';
+ *di++='3';
+ *di++='8';
+ *di++=';';
+ *di++='2';
+ *di++=';';
+ 
+ bufcat("\x1B[38;2;");
+ 
+ radix=10;
+ int_width=1;
+ 
+ bufcat(intstr(r));
+ bufchar(';');
+ 
+ bufcat(intstr(g));
+ bufchar(';');
+
+ bufcat(intstr(b));
+ bufchar('m');
+ bufchar(0);
+
+ bufput(); /*send entire buffer to terminal*/
+ load_radix();
+}
+
+
+
+
+
+
+void move_xy(int x,int y)
+{
+ char *si,*di;
+ save_radix();
+
+ 
+ di=ansi_string;
+ 
+ *di++=0x1B;
+ *di++='[';
+ 
+ radix=10;
+ int_width=1;
+ 
+ si=intstr(y+1);
+ while(*si!=0)
+ {
+  *di++=*si++;
+ }
+ 
+ *di++=';';
+ 
+ si=intstr(x+1);
+ while(*si!=0)
+ {
+  *di++=*si++;
+ }
+
+ *di++='H';
+ *di++=0;
+ 
+  putstring(ansi_string);
+  load_radix();
+
+ }
