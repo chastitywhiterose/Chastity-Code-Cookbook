550f76b790ba4841053f7d2d297658d138ba1c66
Chastity White Rose <chastitywhiterose@gmail.com>
2026-02-01 04:06:24 -0600

    Code Cookbook Update

---
+++ work/hexplore-ansi.asm
@@ -0,0 +1,100 @@
+;this file is dedicated to using ANSI escape codes in Assembly programs.
+;Using special strings of text, it is possible to reposition the cursor in Linux terminal programs
+;Below are some links where I learned about ANSI escape sequences;
+
+;https://notes.burke.libbey.me/ansi-escape-codes/
+;https://gist.github.com/fnky/458719343aabd01cfb17a3a4f7296797
+
+;There will be mostly example strings defined in here. The idea is for reminding myself how these things work.
+
+;changing colors
+ansi_red: db 0x1b,'[1;31m',0
+ansi_green: db 0x1b,'[1;32m',0
+ansi_yellow: db 0x1b,'[1;33m',0
+ansi_blue: db 0x1b,'[1;34m',0
+ansi_magenta: db 0x1b,'[1;35m',0
+ansi_cyan: db 0x1b,'[1;36m',0
+ansi_white: db 0x1b,'[1;37m',0
+
+;this part is important!
+;for maximum speed I embedded some ANSI sequence strings directly into the program
+;Writing dynamic functions by hand was extremely error prone and still required loading the right
+;registers with the arguments to the function. Therefore, unlike with the C programming language, the strings
+;should be included directly. Every location is meant to be hard coded anyway
+
+ansi_home: db 0x1b,'[H',0 ;move cursor to top left (Home)
+ansi_clear: db 0x1b,'[2J',0 ;erase entire screen
+
+;Global variables for x and y geometric positions on the screen
+;These may be used in special escape sequences to reposition the cursor.
+;unlike the hardcoded strings above, the cursor must be movable dynamically
+
+x dd 0
+y dd 0
+
+prefix_x db "x=",0
+prefix_y db "y=",0
+
+ansi_string db 32 dup 0 ;string storage for buildable ansi sequence.
+
+move_cursor:
+
+mov [radix],10    ;set the radix to ten for escape sequences
+mov [int_width],1
+
+mov edi,ansi_string
+mov [edi],byte 0x1B ;all escape sequences start with the escape character.
+inc edi
+
+mov [edi],byte '['
+inc edi
+
+mov eax,[y]
+inc eax
+call intstr ; get the string for y
+
+mov esi,eax ; set source index to the new integer string
+call strcpy
+
+mov [edi],byte ';'
+inc edi
+
+mov eax,[x]
+inc eax
+call intstr ; get the string for x
+
+mov esi,eax ; set source index to the new integer string
+call strcpy
+
+
+mov [edi],byte 'H' ;finish the escape code for setting cursor position
+inc edi
+mov [edi],byte 0 ;terminate the string with zero
+
+mov eax,ansi_string
+call putstring
+
+mov [radix],16
+
+ret
+
+;copies bytes from address edi to esi until a zero is found
+;This is not optimized for speed because it doesn't use the special x86 instructions for that purpose.
+;However, it is easily portable to any CPU I might write assembly for, if I choose which registers to use for that purpose.
+;However, since this is an x86 function, I use edi(destination index) and esi(source index) in their traditional contexts
+
+strcpy:
+
+;jmp strcpy_end
+
+mov al,[esi]
+cmp al,0
+jz strcpy_end
+mov [edi],al
+inc edi
+inc esi
+jmp strcpy
+
+strcpy_end:
+ret
+
---
+++ work/main.asm
@@ -0,0 +1,232 @@
+format ELF executable
+entry main
+include 'chastelib32.asm'
+include 'hexplore-ansi.asm'
+
+main:
+
+mov [radix],16         ;can choose radix for integer output!
+mov [int_width],1
+mov [int_newline],0
+
+mov eax,main_string
+call putstring
+mov     eax, 2         ; invoke SYS_FORK (kernel opcode 2)
+int     80h
+cmp     eax, 0         ; if eax is zero we are in the child process
+jz      child          ; jump if eax is zero to child label
+parent:
+mov     eax, program0_msg ; inside our parent process move parentMsg into eax
+call putstring
+;jmp loop_read_keyboard
+
+
+;this is the game loop where were get input and process it accordingly
+loop_read_keyboard:    ;this loop keeps reading from the keyboard
+
+mov eax,ansi_clear ;move to top left of screen
+call putstring
+
+mov eax,ansi_home ;move to top left of screen
+call putstring
+
+call RAM_hexdump
+
+;obtain selected byte for proper indexing changes
+mov ebx,[RAM_y_select]
+shl ebx,4
+add ebx,[RAM_y_select]
+add [RAM+ebx],1;
+
+mov [x],1
+mov [y],17
+
+call move_cursor
+
+mov eax,0              ;zero eax to receive the key value in al
+mov al,[key];          ;move the key pressed last time into al
+call putint            ;print the number of this key
+call putspace          ;print a space to keep it readable
+call putchar           ;print the character in al register
+call putline           ;print a line to make it easier to read
+
+;will pause until a key is pressed
+call getchar           ;call my function that reads a single byte from the keyboard
+
+cmp al,'q'             ;test for q key. q stands for quit in this context
+jz main_end            ;jump to end of program if q was pressed
+call hexplore_input    ;call the function to process the input and operate the editor
+jmp loop_read_keyboard ;continue the game loop
+
+main_end:
+mov eax, 1  ; invoke SYS_EXIT (kernel opcode 1)
+mov ebx, 0  ; return 0 status on exit - 'No Errors'
+int 80h
+
+; This is the end of the parent process or main program
+; The child process below only uses the stty command before returning to the parent proces
+
+child:
+
+;This child process disables line buffer with stty
+
+;execute a command from the child process
+    mov     edx, environment    ; address of environment variables
+    mov     ecx, arguments      ; address of the arguments to pass to the commandline
+    mov     ebx, command        ; address of the file to execute
+    mov     eax, 11             ; invoke SYS_EXECVE (kernel opcode 11)
+    int     80h
+
+;this is the end of the child process which became the stty command and then terminated naturally
+
+main_string db 'Linux Unbuffered Input Template Program',0Ah,0
+
+program0_msg db 'Parent processed forked',0Ah,0
+
+
+
+;The execve call requires the path to a program, arguments passed to that program, and environment variables if relevant
+;these strings are the execve call data
+command         db      '/bin/stty', 0h     ; command to execute
+arg1            db      'cbreak', 0h
+arguments       dd      command
+                dd      arg1                ; arguments to pass to commandline (in this case just one)
+                dd      0h                  ; end the struct
+environment     dd      0h                  ; arguments to pass as environment variables (inthis case none) end the struct
+
+
+
+;key is defined as dword even though only a byte is used
+;this way, it loads into eax without trouble
+key db 0
+
+prefix_k db "k=",0
+
+getchar:
+push ebx
+push ecx
+push edx
+mov edx,1     ;number of bytes to read
+mov ecx,key   ;address to store the bytes
+mov ebx,0     ;read from stdin
+mov eax,3     ;invoke SYS_READ (kernel opcode 3)
+int 80h       ;call the kernel
+xor eax,eax   ;set eax to 0
+mov al,[key]  ;set lowest part of eax to key read
+pop edx
+pop ecx
+pop ebx
+ret
+
+
+RAM db 0x100 dup '?',0
+RAM_address dd 0
+RAM_x_select dd 4
+RAM_y_select dd 4
+
+
+RAM_hexdump:
+
+mov ebx,RAM
+mov ebp,[RAM_address]
+
+mov edx,0 ;the Y value for this loop
+RAM_dump_loop:
+mov ecx,0 ;the X value for this loop
+mov eax,ebp
+mov [int_width],8
+call putint
+call putspace
+mov [int_width],2
+mov eax,0
+dump_byte_row:
+mov al,[ebx]
+
+normal_print:
+call putint
+call putspace
+normal_print_skip:
+
+inc ebx
+inc ecx
+cmp ecx,0x10;
+jnz dump_byte_row
+
+;optionally, print chars after hex bytes
+call RAM_hexdump_text_row
+
+call putline
+add ebp,0x10
+inc edx
+cmp edx,0x10
+jnz RAM_dump_loop
+
+ret
+
+
+
+RAM_hexdump_text_row:
+push eax
+push ebx
+push ecx
+push edx
+sub ebx,0x10
+mov ecx,0
+mov eax,0
+next_char:
+mov al,[ebx]
+
+;if char is below '0' or above '9', it is outside the range of these and is not a digit
+cmp al,0x20
+jb not_printable
+cmp al,0x7E
+ja not_printable
+
+printable:
+;if char is in printable range,leave as is and proceed to next index
+jmp next_index
+
+not_printable:
+mov al,'.' ;otherwise replace with placeholder value
+
+next_index:
+call putchar
+inc ebx
+inc ecx
+cmp ecx,0x10
+jnz next_char
+
+pop edx
+pop ecx
+pop ebx
+pop eax
+
+ret
+
+
+putint_brackets: 
+push eax
+mov al,'['
+call putchar
+pop eax
+call putint
+mov al,']'
+call putchar
+ret
+
+;code I don't know if I will use or not
+
+;;;;;;;;;;;
+;cmp ecx,[RAM_x_select]
+;jz check_selected_phase_0
+;jmp normal_print
+;check_selected_phase_0:
+;cmp edx,[RAM_y_select]
+;jz check_selected_phase_1
+;jmp normal_print
+;check_selected_phase_1:
+;call putint_brackets
+;jmp normal_print_skip
+
+
+
