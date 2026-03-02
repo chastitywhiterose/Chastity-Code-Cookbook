01b88864f0e9d5fa8f82cf9ef1db34f488eb206a
Chastity White Rose <chastitywhiterose@gmail.com>
2026-03-01 21:06:50 -0600

    Code Cookbook Update

--- work/main.c
+++ work/main.c
@@ -1,10 +1,196 @@
 #include <ncurses.h>
 #include "chastelib_ncurses.h"
 
-int main()
+FILE* fp; /*file pointer*/
+int file_address=0;
+int count=0; /*keeps track of how many bytes were last read from file*/
+int eof_char='?'; /*character used by this program to show user the end of file is reached*/
+
+char RAM[0x1000];
+int RAM_address=0;
+int RAM_view_x=0;
+int RAM_view_y=2;
+
+int byte_selected_x=0;
+int byte_selected_y=0;
+
+
+/*outputs the ASCII text to the right of the hex field*/
+void RAM_textdump(int y,int width)
 {
- int key=0; /*default key to 0 which in the context of my program means nothing has been pressed yet.*/
- int a=0,b,c,d; /*variables for this test program*/
+ int a,x=0;
+ int count=16;
+ x=count;
+ while(x<0x10)
+ {
+  putstring("   ");
+  x++;
+ }
+
+ x=0;
+ while(x<count)
+ {
+  a=RAM[RAM_address+x+y*width];
+  if( a < 0x20 || a > 0x7E ){a='.';}
+  putchar(a);
+  x++;
+ }
+
+}
+
+void RAM_hexdump()
+{
+ int x,y;
+ int width=16,height=16;
+ radix=16;
+ 
+ y=0;
+ while(y<height)
+ {
+  int_width=8;
+  radix=16;
+  putint(RAM_address+file_address+y*width);
+  putstring(" ");
+  int_width=2;
+  x=0;
+  while(x<width)
+  {
+  
+   if(x==byte_selected_x&&y==byte_selected_y)
+   {
+    putint(RAM[x+y*width]&0xFF); /*print this byte in the new color*/
+   }
+   else
+   {
+    putint(RAM[x+y*width]&0xFF);
+   }
+ 
+   putstring(" ");
+
+
+   x++;
+  }
+  RAM_textdump(y,width);
+  
+  putstring("\n");
+  y++;
+  move(RAM_view_x,RAM_view_y+y); radix=16;
+ }
+
+}
+
+int key=0,key1,key2;
+
+void input_operate()
+{
+ int width=16,height=16;
+ int x=byte_selected_x;
+ int y=byte_selected_y;
+ int c; /*character used for some operations*/
+
+ /*these secondary keys are used for control characters such as arrow keys*/
+ key1=0;
+ key2=0;
+
+ /*
+  when a special key such as an arrow key is pressed, it actually sends an escape sequence which is really hard to debug by conventional means
+  We have to use getchar immediately to get these characters and branch according to what they are
+  so that we can process them and they won't be mistaken as other characters
+ */ 
+ if(key==0x1B)
+ {
+  key1=getchar();
+  key2=getchar();
+  
+  
+  /*RAM[0x80]=key;
+  RAM[0x81]=key1;
+  RAM[0x82]=key2;*/
+  
+ }
+ 
+ if(key2=='A'){y--;if(y<0){y=15;}}
+ if(key2=='B'){y++;if(y>=height){y=0;}}
+ if(key2=='C'){x++;if(x>=width){x=0;}}
+ if(key2=='D'){x--;if(x<0){x=15;}}
+
+
+ if(key2==0x35)
+ {
+  if(file_address!=0)
+  {
+   /*before changing page, save the modified bytes from this page back to the file*/
+   fseek(fp,file_address,SEEK_SET);
+   fwrite(RAM,1,count,fp);
+   /*change page and read from the correct file position*/
+   file_address-=0x100;
+   fseek(fp,file_address,SEEK_SET);
+   count=fread(RAM,1,0x100,fp);
+   c=count;while(c<0x100){RAM[c]=eof_char;c++;}
+  }
+ }
+
+ 
+ if(key2==0x36)
+ {
+  /*before changing page, save the modified bytes from this page back to the file*/
+  fseek(fp,file_address,SEEK_SET);
+  fwrite(RAM,1,count,fp);
+  /*change page and read from the correct file position*/
+  file_address+=0x100;
+  fseek(fp,file_address,SEEK_SET);
+  count=fread(RAM,1,0x100,fp);
+  c=count;while(c<0x100){RAM[c]=eof_char;c++;}
+ }
+
+ if(key=='+'){RAM[x+y*width]++;}
+ if(key=='-'){RAM[x+y*width]--;}
+ 
+ /*handle hexadecimal number input*/
+ if( key >= '0' && key <= '9' ){c=key-'0';   RAM[x+y*width]<<=4;RAM[x+y*width]|=c;}
+ if( key >= 'a' && key <= 'f' ){c=key-'a'+10;RAM[x+y*width]<<=4;RAM[x+y*width]|=c;}
+  
+ byte_selected_x=x;
+ byte_selected_y=y;
+}
+
+
+int main(int argc, char *argv[])
+{
+ int c; /*used to index the RAM sometimes*/
+
+ if(argc==1)
+ {
+  printf
+  (
+   "Welcome to Hexplore! The tool for exploring a file in hexadecimal!\n\n"
+   "Enter a filename as an argument to this program to read from it.\n"
+   "You will then see an interface where you can modify the bytes of the file\n"
+  );
+  return 0;
+ }
+
+ if(argc>1)
+ {
+  fp=fopen(argv[1],"rb+");
+  if(fp==NULL)
+  {
+   printf("File \"%s\" cannot be opened.\n",argv[1]);
+   return 1;
+  }
+  else
+  {
+   putstring(argv[1]);
+   putstring("\n");
+  }
+ }
+ 
+ /*
+ step 0 complete: file opened before this code executes
+
+ step 1 is initializing ncurses to manage input for the rest of the program
+ we will then attempt to read from the file and launch the main loop
+ */
 
  /*set the default radix and width for integers at the beginning of the program*/ 
  radix=16;
@@ -15,86 +201,78 @@ int main()
  keypad(stdscr, TRUE);		/* We get F1, F2 etc..		*/
  noecho();			/* Don't echo() while we do getch */
 
- /*
-  I use strint to set the variables by strings rather than immediate values directly
-  Doing it this way looks silly, but it is for the purpose of testing the strint function
- */
- b=strint("10"); /*will always be radix*/
- c=b; /*save what the radix was at the beginning. This will be used later.*/
- d=strint("100"); /*will always be radix squared*/
- 
+  /*attempt to read 256 bytes for the first page*/
+ count=fread(RAM,1,0x100,fp);
+ c=count;while(c<0x100){RAM[c]=eof_char;c++;}
+
+ key = getch();
+
  while(key!='q')
  {
-  putstring("Official test suite for the C version of chastelib.\n");
-  putstring("This edition uses ncurses\n");
- 
-  if(key!=0)
-  {
-   printw("char: %c code: %X\n\n", key,key);
-  }
-  else{putstring("\n\n");}
-
-  if(key==KEY_UP){if(b>c){b--;}}
-  if(key==KEY_DOWN){if(b<d){b++;}}
-  if(key==KEY_LEFT){if(b>=c+c){b-=c;}}
-  if(key==KEY_RIGHT){if(b<=d-c){b+=c;}}
- 
-  if(key == KEY_F(1)) /* Check for F1 key. Usually this would display a help message. */
-  {
-   clear();	/*clear the screen before showing help*/
-   printw("F1 Key pressed: Showing help screen\n\n");
-
-   putstring("This program is an ncurses program which also interacts with chastelib.\n\n");
-   putstring("chastelib is library for converting between integers and strings in multiple radices\n\n");
-   putstring("ncurses is library for controlling keyboard input and printing text at specific locations in a terminal.\n\n");
-   
-   putstring("This program allows you to view the binary, hexadecimal, and decimal numbers of 8-bit value\n");
-   putstring("The arrow keys let you scroll through the list. The q key exits the program\n");
-
-   refresh();			/* Print it on to the real screen */
- 
-   key = getch();		/* Wait for user input */
-   clear();	/*clear the screen before ending help*/
-   continue; /*continue to the next phase of the main loop*/
-  }
-
- /*the actual loop that shows the data for 16 numbers at a time*/
- a=b-c;
- while(a<b)
- {
-  radix=2;
-  int_width=8;
-  putint(a);
-  putstring(" ");
+  /*
+   display hexadecimal byte value of key pressed
+   some keys evaluate to the same number
+  */
+  move(0,0);
   radix=16;
   int_width=2;
-  putint(a);
+  putint(key);
   putstring(" ");
-  radix=10;
-  int_width=3;
-  putint(a);
+  putint(key1);
+  putstring(" ");
+  putint(key2);
+  
+  /*display a character based representation of key pressed*/
+  move(9,0);
+  putchar(key);
 
-  if(a>=0x20 && a<=0x7E)
-  {
-   putstring(" ");
-   addch(a);
-  }
+  
+  move(16,0);
+  putstring("Hexplore : Chastity White Rose");  ;
 
-  putstring("\n");
-  a+=1;
- }
+  move(0,19);
+  putstring("Arrows: Select Byte");
+  move(0,20);
+  putstring("0 to f: Enter Hexadecimal");
+  move(60,19);
+  putstring("q: quit");
+  move(27,20);
+  putstring("page up/down: navigate file");
 
+  /*display x and y of selection*/
+  move(57,0);
+  putstring("X=");
+  putint(byte_selected_x);
+  putstring(" Y=");
+  putint(byte_selected_y);
+   
+  move(RAM_view_x,RAM_view_y);
+
+  RAM_hexdump();
+ 
+  move(0,0);
+  
   refresh();			/* Print it on to the real screen */
   key = getch();		/* Wait for user input */
+  /*input_operate();*/
+
   clear();	
 
  }
  
+ /*
+  before closing the file and ending the program, we must write the modified bytes to the file
+  However, we only write (count) bytes to the file so that we don't accidentally add the full
+  256 bytes of the current hex page if they were not in the original file
+ */
+ fseek(fp,file_address,SEEK_SET); /*seek back to the file address for this page*/
+ fwrite(RAM,1,count,fp); /*write count bytes back into the original location they were read from*/
+ 
+ fclose(fp);
+
  endwin();			/* End curses mode */
  printf("Program terminated normally with %c key.\n",key);
  
  return 0;
 }
-
-/* gcc -Wall -ansi -pedantic main.c -o main -lncurses && ./main */
 
