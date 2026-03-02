575dd0a235b0027a12481774822f4b27516a1d60
Chastity White Rose <chastitywhiterose@gmail.com>
2026-03-02 14:55:03 -0600

    Code Cookbook Update

--- code/cpp/chastelib_pp_extensions/chastelib-pp-string/main.cpp
+++ code/cpp/chastelib_pp_extensions/chastelib-pp-string/main.cpp
@@ -7,39 +7,25 @@ using namespace std;
 int main(int argc, char *argv[])
 {
  int a=0,b;
-
  radix=16;
  int_width=1;
-
  putstring("Official test suite for the C++ version of chastelib.\n");
-
- string s="20"; //create C++ string for input integer
- //pass s force the usage of the overloaded strint function from chastelib_string.hpp
+ string s="100"; //create C++ string for input integer
+ //pass s to force the usage of the overloaded strint function
  b=strint(s);
  while(a<b)
  {
-  radix=2;
-  int_width=8;
-  cout << intstr(a,2,8);
-  putstring(" ");
-  radix=16;
-  int_width=2;
-  putint(a);
-  putstring(" ");
-  radix=10;
-  int_width=3;
-  putint(a);
-
+  cout << intstr(a,2,8) << " ";
+  cout << intstr(a,16,2) << " ";
+  cout << intstr(a,10,3);
   if(a>=0x20 && a<=0x7E)
   {
    putstring(" ");
    cout.put(a);
   }
-
   putstring("\n");
   a+=1;
  }
-  
  return 0;
 }
 
