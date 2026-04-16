c2268e1c271d1d35d6f0fcd5d95da2705a82cb44
Chastity White Rose <chastitywhiterose@gmail.com>
2026-04-16 04:00:57 -0500

    Code Cookbook Update

--- work/main.c
+++ work/main.c
@@ -1,25 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdint.h>
 #include "chastelib.h"
 
 int main(int argc, char *argv[])
 {
- /*int v[]={};*/
- int64_t a=0,b=38,c=1;
-
+ FILE* fp; /*file pointer*/
+ int i=0;
+ 
  radix=10;
- int_width=1;
-
-
- while(a<b)
+ 
+ if(argc==1)
  {
-  putint(c);
-  putstr(",\n");
-  c=c+c;
-  a+=1;
+  putstring
+  (
+   "cintfind\n\n"
+   "To open a file for reading:\n\n\tcintfind file\n\n"
+   "To search that file for an integer\n\n\tcintfind file integer\n\n"
+   "The integer can be any number in decimal.\n"
+  );
+  return 0;
  }
-    
+
+ if(argc>1)
+ {
+  fp=fopen(argv[1],"rb");
+  if(fp==NULL)
+  {
+   printf("File \"%s\" cannot be opened.\n",argv[1]);
+   return 1;
+  }
+  else
+  {
+   putstring("file \"");
+   putstring(argv[1]);
+   putstring("\" opened\n");
+  }
+ }
+ 
+ if(argc>2)
+ {
+  i=strint(argv[2]);
+  printf("integer \"%i\" entered as argument to search for\n",i);
+ }
+
  return 0;
 }
 
