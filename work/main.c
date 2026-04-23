e777689c923342215047001e5ded929023a4aa3e
Chastity White Rose <chastitywhiterose@gmail.com>
2026-04-23 05:13:36 -0500

    Code Cookbook Update

--- work/main.c
+++ work/main.c
@@ -74,7 +74,7 @@ int main(int argc, char **argv)
  /*now call a demo function I wrote*/
  /*sdl_chastelib_test_suite();*/
  
- sdl_chastelib_hexram_edit(argc,argv); /*call this function and pass the command args to it*/
+ sdl_chastelib_hexplore(argc,argv); /*call this function and pass the command args to it*/
 
 
  if(0)
