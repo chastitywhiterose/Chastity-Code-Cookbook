36002e7cb4fd65a8697cab100044648444fd73bf
Chastity White Rose <chastitywhiterose@gmail.com>
2026-04-26 06:25:56 -0500

    Code Cookbook Update

---
+++ code/c/std/chastecat/main.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "chastelib.h"
+ 
+int main(int argc, char *argv[])
+{
+ FILE* fp; /*file pointers*/
+ int c=0;
+   
+ if(argc==1)
+ {
+  putstr("chastecat usage:\n\n");
+  putstr(argv[0]);
+  putstr(" filename.txt\n");
+  return 0;
+ }
+
+ if(argc>1)
+ {
+  fp=fopen(argv[1],"rb");
+  if(fp==NULL)
+  {
+   putstr(argv[1]);
+   putstr("\nFailed to open file\n");
+   return 1;
+  }
+  else
+  {
+   putstr(argv[1]);
+   putstr("\n---\n");
+  }
+ }
+
+ while((c=fgetc(fp))!=EOF)
+ {
+  fputc(c,stdout);
+ 
+ }
+ putstr("\n---\nEOF\n");
+
+ return 0;
+}
+
