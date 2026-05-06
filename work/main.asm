f0d902100d06d8b4a91006ab4180e73df62b1064
Chastity White Rose <chastitywhiterose@gmail.com>
2026-05-06 03:13:43 -0500

    Code Cookbook Update

---
+++ work/main.asm
@@ -0,0 +1,15 @@
+%include 'chaste-elf-32.nasm'
+
+mov eax,4   ; invoke SYS_WRITE (kernel opcode 4 on 32 bit systems)
+mov ebx,1   ; write to the STDOUT file
+mov ecx,msg ; pointer/address of string to write
+mov edx,13  ; number of bytes to write
+int 80h
+
+mov eax,1   ; function SYS_EXIT (kernel opcode 1 on 32 bit systems)
+mov ebx,0   ; return 0 status on exit - 'No Errors'
+int 80h
+
+msg db 'Hello World!',0Ah,0
+
+EOF equ $ ; End Of File label
