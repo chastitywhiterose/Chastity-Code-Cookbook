96df0df90d623bdda610460a8366825ef8fef25d
Chastity White Rose <chastitywhiterose@gmail.com>
2026-05-14 00:40:30 -0500

    Code Cookbook Update

--- code/c/unistd/unistd-hello/main.c
+++ code/c/unistd/unistd-hello/main.c
@@ -2,6 +2,6 @@
 
 int main(int argc, char *argv[])
 {
-    write(1, "Hello World\n", 12); /* write "Hello World" to stdout */
-    _exit(0);                      /* exit with error code 0 (no error) */
+ write(1, "Hello World!\n", 13); /* write "Hello World" to stdout */
+ _exit(0);                       /* exit with error code 0 (no error) */
 }
