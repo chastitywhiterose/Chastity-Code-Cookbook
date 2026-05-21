227127630ce2f4593ab3685a78ff4a616d327f80
Chastity White Rose <chastitywhiterose@gmail.com>
2026-05-21 07:07:23 -0500

    Code Cookbook Update

--- work/main.asm
+++ work/main.asm
@@ -2,22 +2,16 @@
 
 .data
 
-# These variables are used by the intstr function to convert an integer to a string
-# and what radix should be used as well as the width (how many leading zeros)
-
-int_string: .space 32 #reserve space for 32 bytes for up to 32 bits if printed in binary
-int_end: .byte 0 #the terminating zero of the integer string
-radix: .byte 2   #the radix the number will be shown in
-int_width: .byte 1 #by default
-
 string0: .asciiz "I am Chastity White Rose\n"
 
 .text
 
 addi s0, zero, string0
-jal putstr
 
-jal putint
+#li t0, 0x21
+#sb t0, 0(s0)
+
+jal putstr
 
 addi    a0, zero, 0     # a0=0  (exit code for OS)
 addi    a7, zero, 93    # a7=93 (exit system call)
@@ -49,81 +43,3 @@ addi a7, zero, 64 # a7=64    (write system call )
 ecall             #          (environment call  )
 
 ret
-
-
-# The intstr function does several things at once and is the foundation for all integer output.
-# It uses the global radix variable to know which radix or number base to use when turning the integer to a string
-# It also uses the global int_width variable to determine how many leading zeros should be used for the string
-# The purpose of this is to make numbers look good when lined up when they are printed in a list.
-# radices 2 to 36 are supported. Digits higher than 9 will be capital letters
-
-intstr:
-
-la t1, int_end # load target index address of lowest digit
-
-addi t1, t1, -1
-
-lb t2, radix(zero)     # load value of radix into t2
-lb t4, int_width(zero) # load value of int_width into t4
-
-li t3, 1         # load current number of digits, always 1
-
-digits_start:
-
-remu t0, s0, t2 # t0=remainder of the previous division
-divu s0, s0, t2 # s0=s0/t2 (divide s0 by the radix value in t2)
-
-li t5, 10 # load t5 with 10 because RISC-V does not allow constants for branches
-blt t0, t5, decimal_digit
-bge t0, t5, hexadecimal_digit
-
-decimal_digit: # we go here if it is only a digit 0 to 9
-addi t0, t0, '0'
-j save_digit
-
-hexadecimal_digit:
-addi t0, t0, -10
-addi t0, t0, 'A'
-
-save_digit:
-sb t0, 0(t1) # store byte from t0 at address t1
-beq s0, zero, intstr_end
-addi t1, t1, -1
-addi t3, t3, 1
-j digits_start
-
-intstr_end:
-
-li t0, '0'
-prefix_zeros:
-bge t3, t4, end_zeros
-addi t1, t1, -1
-sb t0, 0(t1) # store byte from t0 at address t1
-addi t3, t3, 1
-j prefix_zeros
-end_zeros:
-
-mv s0, t1
-
-ret
-
-# this function calls intstr to convert the s0 register into a string
-# then it uses the system specific call to print the string
-# it also uses the stack to save the value of s0 and ra (return address)
-# this way, s0 is restored to the value it had before this function
-# restoring ra is restored because it is modified during calls to other functions
-
-putint:
-addi sp, sp, -8
-sw ra, 0(sp)
-sw s0, 4(sp)
-
-jal intstr
-jal putstr
-
-lw ra, 0(sp)
-lw s0, 4(sp)
-addi sp, sp, 8
-ret
-
-
