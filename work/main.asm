b1a062437c626dfb79ffda189a846c46bbb736c3
Chastity White Rose <chastitywhiterose@gmail.com>
2026-05-22 09:04:32 -0500

    Code Cookbook Update

---
+++ work/main.asm
@@ -0,0 +1,95 @@
+;Linux 32-bit Assembly Source for chastext
+;a basic text search and replace program
+format ELF executable
+entry main
+
+;a reduced form of chastelib without functions this program doesn't use
+include 'chastelib32.asm'
+
+main:
+
+pop eax
+mov [argc],eax ;save the argument count for later
+
+call putint_and_line
+
+cmp dword [argc],2
+ja help_skip ;if more than 1 argument is given, skip the help message and process the other arguments
+
+help:
+mov eax,help_message
+call putstring
+jmp main_end
+help_skip:
+
+pop eax ;pop the arg for program name but discard it
+
+pop eax ;pop the first argument
+call putstr_and_line
+mov [string0],eax
+
+pop eax ;pop the second argument
+call putstr_and_line
+mov [string1],eax
+
+main_end:
+
+mov eax, 1  ; invoke SYS_EXIT (kernel opcode 1)
+mov ebx, 0  ; return 0 status on exit - 'No Errors'
+int 80h
+
+;the strlen and strcmp are named after the equivalent C functions
+;but are written from scratch by me based on their expected behavior
+
+;a function to get the length of string in eax and return the integer in eax
+
+strlen:
+
+mov ebx,eax ; copy eax to ebx. ebx will be used as index to the string
+
+strlen_start: ; this loop finds the length of the string as part of the putstring function
+
+cmp [ebx],byte 0 ; compare byte at address ebx with 0
+jz strlen_end ; if comparison was zero, jump to loop end because we have found the length
+inc ebx
+jmp strlen_start
+
+strlen_end:
+sub ebx,eax ;subtract start pointer from current pointer to get length of string
+
+mov eax,ebx ;copy the string length back to eax
+
+ret
+
+;compare the string at esi to the one at edi
+
+strcmp:
+
+mov eax,0 ;this will be stay zero unless the strings are different
+
+strcmp_start:
+mov bl,[edi]
+cmp bl,0
+jz strcmp_end
+mov bh,[esi]
+cmp bh,0
+jz strcmp_end
+
+inc edi
+inc esi
+
+cmp bl,bh
+jz strcmp_start ;if they are the same, continue to next character
+
+inc eax ;if they were different, eax will be incremented and the function ends
+
+strcmp_end:
+ret
+
+help_message db 'strcmp by Chastity White Rose',0xA
+db 'This is an assembly program to compare two strings.',0xA
+db 'strcmp string0 string1',0xA,0
+
+argc    dd 0
+string0 dd 0
+string1 dd 0
