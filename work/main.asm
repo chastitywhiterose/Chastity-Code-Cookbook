f5a48b15eda95ba78b11e4fcc7715ee2bfc43a56
Chastity White Rose <chastitywhiterose@gmail.com>
2026-05-31 01:23:59 -0500

    Code Cookbook Update

---
+++ work/main.asm
@@ -0,0 +1,23 @@
+org 100h     ;DOS programs start at this address
+
+;this loop will get all the command line arguments and print them on separate lines
+
+call getarg ;this first call will get the command string
+
+arg_loop:
+call getarg
+cmp ax,0 ;did the getarg function return 0?
+jz arg_loop_end ;if ax was zero, there are no args
+call putstring
+call putline
+jmp arg_loop
+arg_loop_end:
+
+ending:
+mov ax,4C00h ; Exit program
+int 21h
+
+include 'getarg.asm'
+include 'chastelib16.asm'
+
+db 0x48 dup 0 ;add extra bytes to make it 512 bytes exactly
