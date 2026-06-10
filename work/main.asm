3ced966c201ef4e06edfdd380f746caaabd80352
Chastity White Rose <chastitywhiterose@gmail.com>
2026-06-10 10:24:04 -0500

    Code Cookbook Update

---
+++ work/main.asm
@@ -0,0 +1,65 @@
+org 100h
+main:
+
+mov word [radix],10
+mov word [int_width],1
+mov [int_newline],0
+
+mov cx,0
+
+mov [array],byte 1
+
+powers_of_two:
+
+;this section prints the digits
+mov bx,[length]
+array_print:
+dec bx
+mov ax,0
+mov al,[array+bx]
+call putint
+cmp bx,0
+jnz array_print
+call putline
+
+;this section adds the digits
+mov dl,0
+mov bx,0
+array_add:
+mov ax,0
+mov al,[array+bx]
+add al,al
+add al,dl
+mov dl,0
+cmp al,10
+jb less_than_ten
+
+sub al,10
+mov dl,1
+
+less_than_ten:
+mov [array+bx],al
+inc bx
+cmp bx,[length]
+jnz array_add
+
+cmp dl,0
+jz carry_is_zero
+
+mov [array+bx],1
+inc [length]
+
+carry_is_zero:
+
+;keeps track of how many times the loop has run
+add cx,1
+cmp cx,32
+jna powers_of_two
+
+mov ax,4C00h
+int 21h
+
+length dw 1
+array db 32 dup 0
+
+include 'chastelib16.asm'
