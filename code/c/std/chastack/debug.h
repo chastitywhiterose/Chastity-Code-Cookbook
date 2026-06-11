75cce468c1b593c9a6ba4ab0585df56f23576680
Chastity White Rose <chastitywhiterose@gmail.com>
2026-06-11 14:45:38 -0500

    Code Cookbook Update

---
+++ code/c/std/chastack/debug.h
@@ -0,0 +1,61 @@
+/*
+a function just for printing the stack
+this is used for debugging
+*/
+void debug_putstack()
+{
+ int x=0;
+ while(x<stack_length)
+ {
+  putstr("[");
+  putint(x);
+  putstr("] ");
+  putint(stack[x]);
+  putstr("\n");
+  x++;
+ }
+}
+
+/*
+a function used for testing the stack
+it pushes, pops, and prints using the debug_putstack function
+*/
+
+int stack_demo(int argc, char **argv)
+{
+
+ /*set the radix used for integer display*/
+ radix=10;
+ int_width=1;
+
+ /*set the stack pointer to where it should start*/
+  esp=stack+stack_length;
+  ebp=esp; /*backup address of esp to ebp*/
+
+ push(1);  
+ push(2);  
+ push(4);  
+ push(8);  
+
+ push(360); /*push a wrong number to the stack*/
+ pop();     /*pop it off the stack to show it is deleted*/
+
+ push(16);  
+ push(32);  
+ push(64);  
+ push(128);  
+ 
+ putstr("printing the stack\n");
+ debug_putstack();
+ putstr("end of debug stack\n");
+ 
+ 
+ while(esp<ebp)
+ {
+  putint(*esp);
+  putstr("\n");
+  esp++;
+ }
+ 
+ return 0;
+}
---
+++ code/c/std/chastack/main.c
@@ -0,0 +1,108 @@
+#include <stdio.h>
+#include "chastelib.h"
+
+#define stack_length 0x10
+int stack[stack_length]; /*stack array of size stack_length*/
+
+/*
+variables named after registers
+
+esp is declared as a pointer because its only purpose in Assembly is managing the stack
+ebp is declared as a pointer to keep track of the original stack pointer address
+
+all other registers are used as normal integers
+*/
+int eax,ebx,ecx,edx,esi,edi,*ebp,*esp;
+
+/*
+a function just for printing the stack
+this is used for debugging
+*/
+void debug_putstack()
+{
+ int x=0;
+ while(x<stack_length)
+ {
+  putstr("[");
+  putint(x);
+  putstr("] ");
+  putint(stack[x]);
+  putstr("\n");
+  x++;
+ }
+}
+
+void push(i)
+{
+ esp--;
+ *esp=i;
+}
+
+int pop()
+{
+ int i=*esp;
+ *esp=0; /*set the value at [esp] to 0 to delete it*/
+ esp++;
+ return i;
+}
+
+
+int main(int argc, char **argv)
+{
+
+ /*set the radix used for integer display*/
+ radix=10;
+ int_width=1;
+
+ /*set the stack pointer to where it should start*/
+  esp=stack+stack_length;
+  ebp=esp; /*backup address of esp to ebp*/
+
+ push(1);  
+ push(2);  
+ push(4);  
+ push(8);  
+
+ push(360); /*push a wrong number to the stack*/
+ pop();     /*pop it off the stack to show it is deleted*/
+
+ push(16);  
+ push(32);  
+ push(64);  
+ push(128);  
+ 
+ putstr("printing the stack\n");
+ debug_putstack();
+ putstr("end of debug stack\n");
+ 
+ 
+ while(esp<ebp)
+ {
+  putint(*esp);
+  putstr("\n");
+  esp++;
+ }
+ 
+ return 0;
+}
+
+
+
+
+
+
+
+
+/* print all command line arguments on a separate line */
+
+
+/*
+ int x=1;
+ while(x!=argc)
+ {
+  putstr(argv[x]);
+  putstr("\n");
+  x++;
+ }
+*/
+
