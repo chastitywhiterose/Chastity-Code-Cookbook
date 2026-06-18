acf9e3358ead684e41a7a201bea3927bf6f8f22b
Chastity White Rose <chastitywhiterose@gmail.com>
2026-06-18 10:21:21 -0500

    Code Cookbook Update

--- code/asm/fasm/linux-64/conversion/chastack-32to64.sh
+++ code/asm/fasm/linux-64/conversion/chastack-32to64.sh
@@ -18,6 +18,7 @@ sed 's/mov rax,1/mov rax,0x3C/g' -i main64.asm
 sed 's/mov rbx,0/mov rdi,0/g' -i main64.asm
 sed 's/int 0x80/syscall/g' -i main64.asm
 #change dword declarations to qword
+sed 's/dword/qword/g' -i main64.asm
 sed 's/ dd / dq /g' -i main64.asm
 sed 's/ rd / rq /g' -i main64.asm
 #assemble the new file
@@ -27,3 +28,5 @@ chmod +x main64
 #this script is meant to convert the chastack program from 32 to 64 bits
 #note that chastelib64.asm is a header that I manually created
 #these sed commands can make most changes but manual review is still required
+#system calling conventions are still different between 32 and 64 bits
+#and I could have mistakes from things I missed
