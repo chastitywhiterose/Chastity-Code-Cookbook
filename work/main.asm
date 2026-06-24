54ee588e060a0bc87f298d24f6e3d32d11617e32
Chastity White Rose <chastitywhiterose@gmail.com>
2026-06-24 17:44:45 -0500

    Code Cookbook Update

--- work/main.asm
+++ work/main.asm
@@ -2,18 +2,17 @@ format ELF executable
 
 main:                     ;the main function of the assembly program
 
-mov eax,string0
-call putstring
-
 mov dword[radix],16       ;I can choose the radix for integer output!
 mov dword[int_width],1    ;and the width of each integer for padded zeros
 
 ;Linux system call to open a file
 
-mov ecx,2        ;open file in read and write mode 
-mov ebx,filename ;filename should be in eax before this function was called
-mov eax,5        ;invoke SYS_OPEN (kernel opcode 5)
-int 80h          ;call the kernel
+mov ecx,2          ;open file in read and write mode 
+mov ebx,filename   ;filename should be in eax before this function was called
+mov eax,5          ;invoke SYS_OPEN (kernel opcode 5)
+int 80h            ;call the kernel
+
+mov dword [fd],eax ;save the file descriptor returned in eax
 
 cmp eax,0
 jns cat_upper ;if eax is not negative/signed there was no error
@@ -25,12 +24,35 @@ call putint_and_space ;print the number in eax (error code) and space
 mov eax,open_error    ;get the error message I defined
 call putstr_and_line  ;print the error message and newline
 
-jmp main_end ;end the program because we failed at opening the file
+jmp main_end          ;end the program because we can't open the file
 
 cat_upper:
 
-mov eax,string0
-call putstring
+;use the read call to read 1 byte from the file
+
+mov eax,3          ;invoke SYS_READ (kernel opcode 3)
+mov ebx,[fd]       ;move the opened file descriptor into EBX
+mov ecx,buf        ;address to store the byte
+mov edx,1          ;number of bytes to read
+int 80h            ;call the kernel
+
+cmp eax,0          ;were zero bytes read?
+jz cat_upper_end   ;if yes, then end the loop
+
+;otherwise we assume 1 byte was read like we requested
+;we print it to standard output so we can see what was in the file
+
+mov eax,4          ;invoke SYS_WRITE (kernel opcode 4 on 32 bit systems)
+mov ebx,1          ;write to stdout
+int 80h            ;call the kernel
+
+jmp cat_upper
+
+cat_upper_end:
+
+mov ebx,[fd]       ;file number to close
+mov eax,6          ;invoke SYS_CLOSE (kernel opcode 6)
+int 80h            ;call the kernel
 
 main_end:
 
@@ -40,7 +62,8 @@ int 80h                   ;system call for 32-bit Linux kernel
 
 include 'chastelib32.asm'
 
-string0 db 'chastelib test suite for Intel 32-bit Assembly on Linux',0Ah,0
+filename db 'openthis.txt',0 ;name of the file
+fd dd 0                      ;a place for the file descriptor
+buf db 0                     ;a buffer containing 1 byte
 
-filename db 'openthis.txt',0
 open_error db 'error opening file',0
