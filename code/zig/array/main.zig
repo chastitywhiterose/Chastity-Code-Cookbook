94d42fc324479e6c8ee0b50df5422f677d2bfd7e
Chastity White Rose <chastitywhiterose@gmail.com>
2026-07-02 10:24:37 -0500

    Code Cookbook Update

--- code/zig/array/main.zig
+++ code/zig/array/main.zig
@@ -8,6 +8,18 @@ var y:usize=a.len; //set y to length of array
 pub fn main() void
 {
 
+ //set all elements of array to 0 with a loop
+ x=0;
+ while(x<y)
+ {
+  a[x]=0;
+  x+=1;
+ }
+
+ a[4]=8; //set this index to 8
+
+ //print all elements with a loop to see results
+ x=0;
  while(x<y)
  {
   std.debug.print("a[{}]={}\n", .{x,a[x]});
