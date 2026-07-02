19c0b0dcad536c2d75304e00b6ea07e22d7dc43e
Chastity White Rose <chastitywhiterose@gmail.com>
2026-07-02 08:41:42 -0500

    Code Cookbook Update

---
+++ code/zig/constant/main.zig
@@ -0,0 +1,11 @@
+const std = @import("std");
+
+const x=3; //constant x defined as 3
+const y=5; //constant x defined as 5
+
+pub fn main() void
+{
+ std.debug.print("Hello, World!\n", .{});
+ std.debug.print("x = {}\ny = {}\n", .{x,y});
+ std.debug.print("result = {}\n", .{x+y});
+}
