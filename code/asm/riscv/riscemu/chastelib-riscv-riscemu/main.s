9b0e2bd59641c65d26bc24aee49a65bfa5191e8e
Chastity White Rose <chastitywhiterose@gmail.com>
2026-07-07 13:52:28 -0500

    Code Cookbook Update

--- code/asm/riscv/riscemu/chastelib-riscv-riscemu/main.s
+++ code/asm/riscv/riscemu/chastelib-riscv-riscemu/main.s
@@ -294,7 +294,7 @@ strint_end:
 ret
 
 ###############################################################################
-# This putstr function is my  most portable function for RISC-V simulators    #
+# This putstr function is my most portable function for RISC-V simulators     #
 # It calculates the length of a zero terminated string before printing it     #
 # This is the same way used in my Intel Assembly programs for DOS and Linux   #
 # This function was written to operate the same in both RARS and riscemu      #
@@ -302,21 +302,20 @@ ret
 
 putstr:
 
-mv t1, s0 # t1 will be used as an index register
+mv t1, s0                       # t1 will be used as an index register
 
 putstr_strlen_start:
-lb t0, 0(t1)                       # load byte into t0 from address of t1
+lb t0, 0(t1)                    # load byte into t0 from address of t1
 beq t0, zero, putstr_strlen_end # if t0==0, then we jump to the end of the loop.
-addi t1, t1, 1                     # go to next byte
+addi t1, t1, 1                  # go to next byte
 j putstr_strlen_start           # jump to start of the loop
 putstr_strlen_end:              
 
-
-addi a0, zero, 1  #a0=1     (STDOUT file number)
-addi a1, s0, 0    #a1=s0    (address of string )
-sub  a2, t1, s0   #a2=t1-s0 (length of string  )
-addi a7, zero, 64 #a7=64    (write system call )
-ecall             #         (environment call  )
+li a0 1                         # STDOUT file number
+mv a1, s0                       # address of string 
+sub a2, t1, s0                  # length of string
+li a7, 64                       # write call number
+ecall                           # environment call
 
 ret
 
