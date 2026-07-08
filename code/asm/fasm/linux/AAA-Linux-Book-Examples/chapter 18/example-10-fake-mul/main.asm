4c6b0eee33069162c5934d6d6703fe7f9e993e56
Chastity White Rose <chastitywhiterose@gmail.com>
2026-07-08 02:27:30 -0500

    Code Cookbook Update

---
+++ code/asm/fasm/linux/AAA-Linux-Book-Examples/chapter 18/example-10-fake-mul/main.asm
@@ -0,0 +1,55 @@
+format ELF executable
+
+main:
+
+mov dword [radix],10
+mov dword [int_width],1
+
+mov edi,6
+mov esi,7
+
+mov eax,edi
+call putint
+call putline
+mov eax,esi
+call putint
+call putline
+
+fake_mul:
+mov eax,0
+fake_mul_add_loop:
+cmp esi,0
+jz fake_mul_add_loop_end
+and esi,1
+jz skip_add
+add eax,edi
+skip_add:
+shl edi,1
+shr esi,1
+jmp fake_mul_add_loop
+fake_mul_add_loop_end:
+
+mov eax,edi
+call putint
+call putline
+mov eax,esi
+call putint
+call putline
+
+mov eax,1
+mov ebx,0
+int 0x80
+
+include 'chastelib32.asm'
+
+;int mul(int di,int si)
+;{
+; int ax=0;
+; while(si!=0)
+; {
+;  if((si&1)!=0){ax=add(ax,di);}
+;  di<<=1;
+;  si>>=1;
+; }
+; return ax;
+;}
