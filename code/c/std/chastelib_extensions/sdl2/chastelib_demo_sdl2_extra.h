9cfc235c497713a7ad294d7c5005c17787c3d528
Chastity White Rose <chastitywhiterose@gmail.com>
2026-07-12 01:44:43 -0500

    Code Cookbook Update

--- code/c/std/chastelib_extensions/sdl2/chastelib_demo_sdl2_extra.h
+++ code/c/std/chastelib_extensions/sdl2/chastelib_demo_sdl2_extra.h
@@ -21,3 +21,66 @@ int demo_galatians()
 
  return 0;
 }
+
+
+int demo_power2()
+{
+ int a=0,b=30;
+ int x,y;
+ #define length 1000
+ int length2=20;
+ char c[length];
+ 
+ radix=10; /*set the radix we will use*/
+ 
+ main_font.char_scale=3; 
+
+ 
+   sdl_clear();  /*clear the screen before we begin writing*/
+
+ x=0;
+ while(x<length)
+ {
+  c[x]=0;
+  x++;
+ }
+ c[0]=1;
+
+ while(a<=b)
+ {
+  
+
+  x=length2;
+  while(x>0)
+  {
+   x--;
+   putint(c[x]);
+  }
+  
+  /*optionally, print which power of two was printed this line*/
+  putstr(" = ");
+  putstr("2^");
+  putint(a);
+
+  putstr("\n");
+
+  y=0;
+  x=0;
+  while(x<=length2)
+  {
+   c[x]+=c[x];
+   c[x]+=y;
+   if(c[x]>9){y=1;c[x]-=10;}else{y=0;}
+   x++;
+  }
+  if(c[length2]>0){length2++;}
+
+  a++;
+ }
+
+ SDL_UpdateWindowSurface(window); /*update window to show the results*/
+ sdl_wait_escape(); /*wait till escape key pressed*/
+
+ return 0;
+}
+
