90b1bd94d7c0298aeb4f3e9fb3da2fcd589a06bd
Chastity White Rose <chastitywhiterose@gmail.com>
2026-07-16 09:33:54 -0500

    Code Cookbook Update

--- work/main.pas
+++ work/main.pas
@@ -1,26 +1,23 @@
 program pow2;
 
 var
- a:integer;
- b:integer;
- x:integer;
- y:integer;
+ a,b,x,y:integer;
  c:array[0..255] of integer;
  length:integer=20; //minimum length for digits
 
 begin;
  a:=0;
- b:=64;
+ b:=64; //highest exponent we want
 
- //set digits to 0
  x:=0;
  while x<length  do
  begin
-  c[x]:=0;
+  c[x]:=0; //set digits to 0
   x:=x+1;
  end;
  c[0]:=1; //but set lowest to 1
 
+ //main loop
  while a<=b do
  begin;
 
@@ -34,29 +31,19 @@ begin;
  //write power notation and a newline
  writeln(' 2^',a); 
 
-  y:=0;
-  x:=0;
-  while x<=length do
-  begin
-   c[x]:=c[x]+c[x];
-   c[x]:=c[x]+y;
-   if c[x]>9 then
-   begin
-    y:=1;
-    c[x]:=c[x]-10;
-   end
-   else
-   begin
-    y:=0;
-   end;
+ y:=0;
+ x:=0;
+ while x<=length do
+ begin
+  c[x]:=c[x]+c[x];
+  c[x]:=c[x]+y;
+  if c[x]>9 then begin y:=1; c[x]:=c[x]-10; end
+  else begin y:=0; end;
    x:=x+1;
   end;
-  if c[length]>0 then
-  begin
-   length:=length+1;
-  end;
+  if c[length]>0 then begin length:=length+1; end;
 
- a:=a+1;
+  a:=a+1;
  end;
 
 end.
