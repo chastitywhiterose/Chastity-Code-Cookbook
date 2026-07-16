6a506004aa21b505f4467bd80c3a08fdfc06e81d
Chastity White Rose <chastitywhiterose@gmail.com>
2026-07-16 06:54:47 -0500

    Code Cookbook Update

---
+++ work/main.pas
@@ -0,0 +1,12 @@
+program EatAtJoes;
+
+uses Crt;
+
+begin
+ ClrScr;
+ GotoXY(15,11);
+ WriteLn('Eat at Joe''s!');
+ GotoXY(15,12);
+ WriteLn('Ten Million Flies Can''t ALL Be Wrong!');
+ Readln;
+end.
