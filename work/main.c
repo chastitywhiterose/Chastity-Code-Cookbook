c88e054ec64fc1476cbc38277a86ead6625ed506
Chastity White Rose <chastitywhiterose@gmail.com>
2026-07-19 10:30:51 -0500

    Code Cookbook Update

---
+++ work/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+ printf("Hello World!\n");
+ return 0;
+}
+/*
+ This is a C program.
+ It prints a hello message.
+
+ compile and run as:
+
+ gcc main.c -o main && ./main
+*/
