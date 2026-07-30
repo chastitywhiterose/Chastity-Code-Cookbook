2d7a2f3f855495d0c1c761eb4d599dfbe6e82d81
Chastity White Rose <chastitywhiterose@gmail.com>
2026-07-30 10:21:56 -0500

    Code Cookbook Update

--- code/asm/fasm/linux/AAA-Linux-Book-Examples/chapter 8/fasm-keyboard-input/main.asm
+++ code/asm/fasm/linux/AAA-Linux-Book-Examples/chapter 8/fasm-keyboard-input/main.asm
@@ -29,6 +29,7 @@ mov eax,string2
 call putstring
 
 mov eax,[count]
+dec eax
 call putint
 call putline
 
@@ -46,6 +47,20 @@ string3 db 'exit',0
 
 buf db 0x100 dup '?'
 count dd 0
+last_char db 0
+
+;summary
+;the getstring function is the reverse function of putstring
+;instead of printing a string to standard output
+;it reads a string from standard input (AKA the keyboard)
+
+;details
+;the getstring function is designed to get a string of text
+;which is terminated by whitespace or any non printable character
+;the idea is that multiple strings can be passed on one line
+;separated by spaces, similar to command line arguments
+;this function was written for the specific purpose of converting any of
+;my programs that used command line arguments to read from stdin instead
 
 getstring:
 
@@ -59,36 +74,42 @@ mov ebx,0     ;read from stdin
 mov eax,3     ;invoke SYS_READ (kernel opcode 3)
 int 80h       ;call the kernel
 
-cmp eax,1     ;was 1 character read?
-jnz getstring_end ; if not, then end this loop
+cmp eax,1             ;was 1 character read?
+jz getstring_read_yes ;if yes, process this character and add to string
+ret                   ;otherwise exit the function now
 
-mov al,[ecx]  ;mov last character read into al register
+getstring_read_yes:
+add [count],eax   ;add how many characters we have read
+mov al,[ecx]      ;mov last character read into al register
 
 ;check if this character is in the proper range to be part of the string
 
-cmp al,0x20      ;compare with 0x20 (space)
+cmp al,0x21      ;compare with 0x21 (!=exclamation)
 jb getstring_end ;jump if below to getstring_end label
 cmp al,0x7E      ;compare with 0x7E (tilde)
 ja getstring_end ;jump if above to getstring_end label
 
 ;if neither jump happened, keep the character and
 
-inc [count]   ;increment how many characters we have read
 inc ecx       ;increment address where next byte is read from
 jmp getstring_chars ;jump back to start of loop and keep reading
 
 getstring_end:
 
+mov [last_char],al ;save the last character read
 mov byte[ecx],0 ;terminate this string with a zero
 
 mov eax,buf ;mov the buffer address to eax for returning the string
 
 ret
 
+
+;Short Description of strcmp:
 ;strcmp compares the string at esi to the one at edi
-;eax returns 0 if the strings are the same and 1 if different
+;eax returns 0 if the strings are the same and non zero if different
 ;the algorithm is simple but I will explain it for those who are confused
 
+;Long Description of strcmp:
 ;eax is initialized to zero
 ;a byte from each string is loaded into the al and bl registers
 ;the bytes are compared. if they are different, then we jump to the end
@@ -102,6 +123,7 @@ ret
 ;ebx,esi,and edi are preserved but eax is the return value
 ;also, the sub instruction at the end of the function also updates the flags
 ;so you can "jz" or "jnz" to a label after calling this function based on results
+;This makes strcmp as useful for strings as the Intel "cmp" instruction is for integers
 
 strcmp:
 
