0c2a34640fc748c70d320d86383220e3eb3ad4f5
Chastity White Rose <chastitywhiterose@gmail.com>
2026-08-02 06:52:41 -0500

    Code Cookbook Update

--- code/pascal/chastelib_core/main.pas
+++ code/pascal/chastelib_core/main.pas
@@ -1,4 +1,4 @@
-program hexadecimal;
+program chastelib;
 
 const
  string0='Official test suite for the Pascal version of chastelib.'#10;
@@ -9,6 +9,7 @@ var //this is the global variable section
  
  radix:integer;       //current radix being used
  int_width:integer=1; //global integer width
+ strint_errors:integer=0; //error result for strint function
 
 (*
 A function to print a string using Pascal's write function.
@@ -60,11 +61,77 @@ begin
  putstr(intstr(i));
 end;
 
-begin
- a:=0;
- b:=256;
+(*
+Because characters and integers are separate types in Pascal,
+it is required to get the ASCII value of characters in the string
+for the strint function so I can do the math the same way
+as I did in the C version of the function.
+*)
 
- radix:=16; //set the radix used by intstr to 16
+function strint(s:string):integer;
+var
+ i:integer=0; //integer that will be built and returned from this function
+ x:integer=1; //index used to scan forward through the string
+ c:integer=0;
+begin
+ strint_errors := 0; (*set zero errors before we parse the string*)
+ if (radix<2) or (radix>36 ) then
+ begin
+  strint_errors+=1;
+  writeln('Error: radix ',radix,' is out of range!');
+ end;
+ while(x<=length(s)) do
+ begin
+  c:=ord(s[x]);
+  if (c>=ord('0')) and (c<=ord('9')) then 
+  begin
+   c-=ord('0')
+  end
+  else if (c>=ord('A')) and (c<=ord('Z')) then
+  begin
+   c-=ord('A');
+   c+=10;
+  end
+  else if (c>=ord('a')) and (c<=ord('z')) then
+  begin
+   c-=ord('a');
+   c+=10;
+  end
+  
+  else if (c < $21 ) then
+  begin
+   break; (*end loop because we have found whitespace*)
+  end
+  
+  else
+  begin
+   strint_errors+=1;
+   writeln('Error: ',s[x],' is not an alphanumeric character!');break;
+  end;
+  
+  if(c>=radix) then
+  begin
+   strint_errors+=1;
+   writeln('Error: ',s[x],' is not a valid character for radix ',radix);
+   break;
+  end;
+  
+  i*=radix; //multiply by the radix
+  i+=c;     //add the digit from the character processed
+
+  x:=x+1;
+ end;
+ strint:=i;
+end;
+
+
+
+
+begin
+ radix:=16; //set the radix used by both intstr and strint functions
+
+ a:=0;
+ b:=strint('100');
 
  putstr(string0);
 
@@ -99,3 +166,4 @@ end.
 (*
  fpc main.pas && ./main
 *)
+
