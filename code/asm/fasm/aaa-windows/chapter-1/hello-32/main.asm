6095766fb307491ed1ccc2ecfc3ca0913d29fcec
Chastity White Rose <chastitywhiterose@gmail.com>
2026-09-09 16:57:54 -0600

    Fixed order of argument passing in hello examples for 32 and 64 bit windows

--- code/asm/fasm/aaa-windows/chapter-1/hello-32/main.asm
+++ code/asm/fasm/aaa-windows/chapter-1/hello-32/main.asm
@@ -1,7 +1,7 @@
 format PE console
 entry main
 
-include 'win32ax.inc'       ;includes standard Windows 32-bit definitions and macros
+include 'win32a.inc' ;include Windows 32-bit macros
 
 main:
 
@@ -12,12 +12,9 @@ call putstring
 push 0             ;exit code for operating system
 call [ExitProcess] ;Exit the process with code 0
 
-;A string to test if output works
 main_string db 'Hello World',0x0D,0x0A,0
 
-write_count dd 0        ;variable to store how many bytes were written
-
-putstring:              ;print string pointed to by eax register
+putstring:         ;print string pointed to by eax register
 
 push eax
 push ebx
@@ -36,24 +33,25 @@ jmp putstring_strlen_start
 putstring_strlen_end:
 sub ebx,eax ;subtract start pointer from current pointer to get length of string
 
-;Write string using Win32 WriteFile system call.
-push 0              ;Optional Overlapped Structure
-push write_count    ;address to store how many bytes are written
-push ebx            ;Number of bytes to write
-push eax            ;address of string to print
-push -11            ;STD_OUTPUT_HANDLE = Negative Eleven
-call [GetStdHandle] ;use the above handle
-push eax            ;eax is return value of previous function
-call [WriteFile]    ;all the data is in place, do the write thing!
+;Windows 32-bit WriteFile system call
+
+push 0               ;lpOverlapped = NULL
+push 0               ;lpNumberOfBytesWritten = NULL
+push ebx             ;nNumberOfBytesToWrite = ebx
+push eax             ;lpBuffer = address of string to write
+push -11             ;STD_OUTPUT_HANDLE = Negative Eleven
+call [GetStdHandle]  ;Get Standard Handle for -11
+push eax             ;hFile = eax (returned from GetStdHandle)
+call [WriteFile]
+
 
 pop edx
 pop ecx
 pop ebx
 pop eax
 
-ret ;this is the end of the putstring function return to calling location
+ret
 
-;FASM builds the Import Address Table (IAT) directly in the source file
 section '.idata' import data readable writeable
 
 library kernel32, 'KERNEL32.DLL'
--- code/asm/fasm/aaa-windows/chapter-1/hello-64/main.asm
+++ code/asm/fasm/aaa-windows/chapter-1/hello-64/main.asm
@@ -1,7 +1,7 @@
 format PE64 console
 entry main
 
-include 'win64ax.inc'       ;includes standard Windows 64-bit definitions and macros
+include 'win64a.inc' ;include Windows 64-bit macros
 
 main:
 
@@ -12,12 +12,9 @@ sub rsp,40         ;align stack (required in windows 64-bit)
 mov rcx,0          ;exit code for operating system
 call [ExitProcess] ;Exit the process with code 0
 
-;A string to test if output works
 main_string db 'Hello World',0x0D,0x0A,0
 
-write_count dq 0        ;variable to store how many bytes were written
-
-putstring:              ;print string pointed to by rax register
+putstring:         ;print string pointed to by rax register
 
 push rax
 push rbx
@@ -36,30 +33,25 @@ jmp putstring_strlen_start
 putstring_strlen_end:
 sub rbx,rax ;subtract start pointer from current pointer to get length of string
 
-sub rsp,40  ;align stack before Win API functions(required in windows 64-bit)
-
-mov rdx,rax ;pointer to message
-
-mov rcx, -11        ; STD_OUTPUT_HANDLE
-call [GetStdHandle] ; Get Standard Output Handle
-mov rcx,rax         ; copy handle to ecx
-
-mov r8,rbx          ;message length
-mov r9,write_count  ;address to store how many bytes are written
-
-mov qword [rsp + 32], 0 ; Parameter 5: Must be placed on the stack
+;Windows 64-bit WriteFile system call
+sub rsp,40           ;align stack for Win64 API calls
+mov qword [rsp+32],0 ;lpOverlapped = NULL
+mov r9,0             ;lpNumberOfBytesWritten = NULL
+mov r8,rbx           ;nNumberOfBytesToWrite = rbx
+mov rdx,rax          ;lpBuffer = address of string to write
+mov rcx, -11         ;STD_OUTPUT_HANDLE = Negative Eleven
+call [GetStdHandle]  ;Get Standard Handle for -11
+mov rcx,rax          ;hFile = rax (returned from GetStdHandle)
 call [WriteFile]
-
-add rsp,40  ;restore stack now that WinAPI calls are done
+add rsp,40           ;restore stack now that WinAPI calls are done
 
 pop rdx
 pop rcx
 pop rbx
 pop rax
 
-ret ;this is the end of the putstring function return to calling location
+ret
 
-;FASM builds the Import Address Table (IAT) directly in the source file
 section '.idata' import data readable writeable
 
 library kernel32, 'KERNEL32.DLL'
